435530fb65b728165aa6fed5971c725426377d33
ToanTran <toantdfu@gmail.com>
2022-02-12 06:19:17 +0900

    Time: 99 ms (41.00%), Space: 42.3 MB (22.74%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,31 +3,38 @@
  * @param {number} target
  * @return {number[]}
  */
-const binarySearch = (nums, target, isFindFirst = true) => {
-    let l = 0
-    let r = nums.length - 1
-    
-    while (l <= r) {
-        const mid = Math.floor((l + r) / 2)
-        
-        if (nums[mid] === target) {
-            if ((isFindFirst && nums[mid - 1] !== target) || (!isFindFirst && nums[mid + 1] !== target)) {
-                return mid
-            } else {
-                if (isFindFirst) {
-                    r = mid - 1
-                } else {
-                    l = mid + 1
-                }
-            }
-        } else if (target > nums[mid]) {
-            l = mid + 1
-        } else {
-            r = mid - 1
-        }
-    }
-    
-    return -1
-}
+const binarySearch = (nums, target, isFindFirst = true, l = 0) => {
+  if (l === -1) return l;
+  let r = nums.length - 1;
 
-const searchRange = (nums, target) => [binarySearch(nums,target), binarySearch(nums,target,false)]
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+
+    if (nums[mid] === target) {
+      if (
+        (isFindFirst && nums[mid - 1] !== target) ||
+        (!isFindFirst && nums[mid + 1] !== target)
+      ) {
+        return mid;
+      } else {
+        if (isFindFirst) {
+          r = mid - 1;
+        } else {
+          l = mid + 1;
+        }
+      }
+    } else if (target > nums[mid]) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+const searchRange = (nums, target) => {
+  const first = binarySearch(nums, target);
+  const last = binarySearch(nums, target, false, first);
+  return [first, last];
+};
