424ccaddfc999325722fa667aac747221c30b675
ToanTran <toantdfu@gmail.com>
2022-02-13 06:15:59 +0900

    Time: 94 ms (44.72%), Space: 42.1 MB (18.00%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.js
+++ 162-find-peak-element/162-find-peak-element.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 const findPeakElement = nums => {
-    if (nums.length === 1) return 0
-    if (nums.length === 2) return nums[0] > nums[1] ? 0 : 1
+    
     let l = 0
     let r = nums.length - 1
     
     while (l <= r) {
         const mid = Math.floor((l + r) / 2)
         
-        if (nums[mid] > (nums[mid + 1] || 0) && nums[mid] > (nums[mid - 1] || 0)) {
-            return mid
-        } else if (nums[mid - 1] > nums[mid + 1]) {
-            r = mid - 1
-        } else {
+        if (nums[mid] < nums[mid + 1]) {
             l = mid + 1
+        } else {
+            r = mid - 1
         }
     }
     
+    return l
 };
