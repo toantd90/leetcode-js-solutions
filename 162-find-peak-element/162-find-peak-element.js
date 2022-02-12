6b3c12d8ecd36e24417dc2f7fad1940f1e2c3e00
ToanTran <toantdfu@gmail.com>
2022-02-13 06:42:05 +0900

    Time: 82 ms (56.35%), Space: 42.1 MB (18.00%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.js
+++ 162-find-peak-element/162-find-peak-element.js
@@ -2,22 +2,35 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findPeakElement = nums => {
-    if (nums.length === 1) return 0
-    if (nums.length === 2) return nums[0] > nums[1] ? 0 : 1
-    let l = 0
-    let r = nums.length - 1
-    
-    while (l <= r) {
-        const mid = Math.floor((l + r) / 2)
-        
-        if (nums[mid] > (nums[mid + 1] || 0) && nums[mid] > (nums[mid - 1] || 0)) {
-            return mid
-        } else if (nums[mid - 1] > nums[mid + 1]) {
-            r = mid - 1
-        } else {
-            l = mid + 1
-        }
+const findPeakElement = (nums) => {
+  let l = 0;
+  let r = nums.length - 1;
+
+  // Intuition
+  // while (l <= r) {
+  //   const mid = Math.floor((l + r) / 2);
+
+  //   if (
+  //     nums[mid] > (nums[mid + 1] || -2e31) &&
+  //     nums[mid] > (nums[mid - 1] || -2e31)
+  //   ) {
+  //     return mid;
+  //   } else if (nums[mid - 1] > nums[mid + 1]) {
+  //     r = mid - 1;
+  //   } else {
+  //     l = mid + 1;
+  //   }
+  // }
+
+  while (l <= r) {
+    const mid = l + Math.floor((r - l) / 2);
+
+    if (nums[mid] < nums[mid + 1]) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
     }
-    
+  }
+
+  return l;
 };
