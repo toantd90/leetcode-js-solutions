8b5c1600002b33b6a974ae2268cba0fab2cfab03
ToanTran <toantdfu@gmail.com>
2022-02-13 11:37:18 +0900

    Time: 973 ms (18.43%), Space: 80.6 MB (5.02%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = nums => {
+    nums.sort((n1, n2) => n1 - n2)
+    const len = nums.length
+    let res = new Set()
+    for (let i = 0; i < len; i++) {
+        const sum = -nums[i]
+        let l = i + 1
+        let r = len - 1
+        
+        if (l > r - 1) continue
+        else {
+            while (l <= r - 1) {
+                const curSum = nums[l] + nums[r]
+                if (curSum === sum) {
+                    res.add(`${nums[i]}*${nums[l]}*${nums[r]}`)
+                    r--
+                } else if (curSum > sum) {
+                    r--
+                } else {
+                    l++
+                }
+            }
+        }
+    }
+    console.log(res)
+    return Array.from(res).map(el => el.split('*'))
+};
