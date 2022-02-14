7a0c0771c3d49c79cba3b7ba48edcf8a90300b40
ToanTran <toantdfu@gmail.com>
2022-02-14 11:45:23 +0900

    Time: 95 ms (49.59%), Space: 42.9 MB (14.32%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.js
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -3,27 +3,19 @@
  * @param {string} t
  * @return {boolean}
  */
-const isBackSpace = char => char === '#'
+const isBackcspace = char => char === '#';
 
-const backspaceCompare = (s, t) => {
-    let p1 = 0
-    let p2 = 0
-    let s1 = []
-    let s2 = []
-    
-    while (p1 < s.length || p2 < t.length) {
-        if (p1 < s.length) {
-          if (isBackSpace(s[p1])) s1.pop()
-          else s1.push(s[p1])
-          p1++
-        }
-        
-        if (p2 < t.length) {
-          if (isBackSpace(t[p2])) s2.pop()
-          else s2.push(t[p2])
-          p2++
-        }
-    }
-    
-    return s1.join('') === s2.join('')
+const backspace = s => {
+  let p = 0;
+  let stack = [];
+
+  while (p < s.length) {
+    if (isBackcspace(s[p])) stack.pop();
+    else stack.push(s[p]);
+    p++;
+  }
+
+  return stack.join('');
 };
+
+const backspaceCompare = (s, t) => backspace(s) === backspace(t);
