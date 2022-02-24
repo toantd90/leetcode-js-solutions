e77aed86644549eb6e065d147599a7082d501fcc
ToanTran <toantdfu@gmail.com>
2022-02-24 09:32:46 +0900

    Time: 80 ms (91.34%), Space: 45.8 MB (9.89%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -1,45 +1,24 @@
-// /**
-//  * @param {number[]} nums
-//  * @return {number[][]}
-//  */
-// const swap = (nums, i, j) => {
-//     if (i !== j)
-//         [nums[i], nums[j]] = [nums[j], nums[i]]
-// }
-// const permutation = (nums, l, r, res) => {
-//     if (l === r) {
-//         res.push([...nums])
-//     } else {
-//         for (let i = l; i < r; i++) {
-//             swap(nums, i, l)
-//             permutation(nums, l + 1, r, res)
-//             swap(nums, i, l)
-//         }
-//     }
-// }
-// const permute = nums => {
-//     let res = []
-//     permutation(nums, 0, nums.length, res)
-    
-//     return res
-// };
-
-const backtracking = (res, tempList, nums) => {
-    if (tempList.length === nums.length) {
-        res.push([...tempList])
+const backtrack = (res, list, nums) => {
+    if (list.length === nums.length) {
+        res.push([...list])
     } else {
         for (let i = 0; i < nums.length; i++) {
-            if (tempList.includes(nums[i])) continue
-            tempList.push(nums[i])
-            backtracking(res, tempList, nums)
-            tempList.pop()
+            if (list.includes(nums[i])) continue
+            list.push(nums[i])
+            backtrack(res, list, nums)
+            list.pop()
         }
     }
 }
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 const permute = nums => {
     let res = []
-    backtracking(res, [], nums)
+    
+    backtrack(res, [], nums)
     
     return res
 };
