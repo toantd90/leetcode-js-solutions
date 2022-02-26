7026e149711c6cc12364d0731cb9fcbb8bee2d60
ToanTran <toantdfu@gmail.com>
2022-02-27 08:25:58 +0900

    Time: 74 ms (75.61%), Space: 42.6 MB (39.02%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -4,22 +4,17 @@
  */
 const maxLengthBetweenEqualCharacters = s => {
     let firstIndex = new Array(26).fill(-1)
-    let lastIndex = new Array(26).fill(-1)
+    let res = -1
+    
     
     for (let i = 0; i < s.length; i++) {
         const index = s.charCodeAt(i) - 97
         
         if (firstIndex[index] === -1) {
             firstIndex[index] = i
+        } else {
+            res = Math.max(res, i - firstIndex[index] - 1)
         }
-        lastIndex[index] = i
-    }
-    
-    let res = -1
-    
-    for (let i = 0; i < 26; i++) {
-        if (firstIndex[i] === -1 || lastIndex[i] === -1) continue
-        res = Math.max(res, lastIndex[i] - firstIndex[i] - 1)
     }
     
     return res
