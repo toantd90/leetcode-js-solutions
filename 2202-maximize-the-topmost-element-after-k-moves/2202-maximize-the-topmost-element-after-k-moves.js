824aa95c7ec3980f9a9686350c7870b2d3d5e769
ToanTran <toantdfu@gmail.com>
2022-03-13 13:31:26 +0900

    Time: 139 ms (100.00%), Space: 52.5 MB (100.00%) - LeetHub

---
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const maximumTop = (nums, k) => {
+    if (k === 0) return nums[0]
+    if (k === 1) return nums[1] !== undefined ? nums[1] : -1
+    if (nums.length === 1) return k % 2 === 0 ? nums[0] : -1
+    if (nums.lengh === 2) return nums[1]
+    
+    let maxValue = nums[0]
+    for (let i = 1; i < Math.min(k - 1, nums.length); i++) {
+        if (nums[i] > maxValue) {
+            maxValue = nums[i]
+        }
+    }
+    if (k < nums.length) {
+        return Math.max(maxValue, nums[k])
+    }
+    
+    return maxValue
+    
+};
