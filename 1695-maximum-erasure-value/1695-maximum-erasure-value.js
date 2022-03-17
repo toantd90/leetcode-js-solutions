841002795fd46e92eac772b22e9df7b1452dceff
ToanTran <toantdfu@gmail.com>
2022-03-17 14:49:13 +0900

    Time: 188 ms (72.60%), Space: 57.5 MB (79.45%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maximumUniqueSubarray = nums => {
+  let l = 0,
+    r = 0,
+    count = {},
+    cur = 0,
+    max = 0;
+
+  while (r < nums.length) {
+    if (count[nums[r]] === 1) {
+      while (count[nums[r]] === 1 && l < r) {
+        cur -= nums[l];
+        count[nums[l]]--;
+        l++;
+      }
+    } else {
+      count[nums[r]] = 1;
+      cur += nums[r];
+      max = Math.max(max, cur);
+      r++;
+    }
+  }
+
+  return max;
+};
