6431702b970126ed81a7d70ba0dd2e6ff113adcd
ToanTran <toantdfu@gmail.com>
2022-03-21 07:23:04 +0900

    Time: 98 ms (100.00%), Space: 42.1 MB (100.00%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.js
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.js
@@ -2,19 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-const countHillValley = nums => {
-    let uniqueNums = [nums[0]]
-    
-    for (let i = 1; i < nums.length; i++) {
-        const topEle = uniqueNums[uniqueNums.length - 1]
-        if (nums[i] !== topEle) uniqueNums.push(nums[i])
-    }
-    
-    let cnt = 0
-    for (let i = 1; i < uniqueNums.length - 1; i++) {
-        if ((uniqueNums[i] > uniqueNums[i - 1] && uniqueNums[i] > uniqueNums[i + 1]) || (uniqueNums[i] < uniqueNums[i - 1] && uniqueNums[i] < uniqueNums[i + 1]))
-            cnt++
-    }
-    
-    return cnt
+const countHillValley = (nums) => {
+  let uniqueNums = [nums[0]];
+
+  for (let i = 1; i < nums.length; i++) {
+    const topEle = uniqueNums[uniqueNums.length - 1];
+    if (nums[i] !== topEle) uniqueNums.push(nums[i]);
+  }
+
+  let cnt = 0;
+  for (let i = 1; i < uniqueNums.length - 1; i++) {
+    if (
+      (uniqueNums[i] > uniqueNums[i - 1] &&
+        uniqueNums[i] > uniqueNums[i + 1]) ||
+      (uniqueNums[i] < uniqueNums[i - 1] && uniqueNums[i] < uniqueNums[i + 1])
+    )
+      cnt++;
+  }
+
+  return cnt;
 };
