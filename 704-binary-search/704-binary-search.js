f18e95cc63da4731c47168708b443bfad051c078
ToanTran <toantdfu@gmail.com>
2022-04-02 07:39:38 +0900

    Time: 136 ms (5.26%), Space: 45.2 MB (53.18%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,20 +4,19 @@
  * @return {number}
  */
 const search = (nums, target) => {
-    let left = 0
-    let right = nums.length - 1
+    let l = 0, r = nums.length - 1
     
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2)
+    while (l <= r) {
+        const mid = l + Math.floor((r - l) / 2)
         
         if (nums[mid] === target) {
             return mid
         } else if (nums[mid] > target) {
-            right = mid - 1
+            r = mid - 1
         } else {
-            left = mid + 1
+            l = mid + 1
         }
     }
     
     return -1
-}
+};
