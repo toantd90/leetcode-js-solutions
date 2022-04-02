a23d391056649f9c29b9d617a6c37e7604a7e2af
ToanTran <toantdfu@gmail.com>
2022-04-03 08:18:51 +0900

    Time: 117 ms (5.79%), Space: 42.4 MB (32.38%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -11,14 +11,12 @@ const searchInsert = (nums, target) => {
         
         if (nums[mid] === target) {
             return mid
-        } else if ((mid === nums.length - 1 || nums[mid + 1] > target) && nums[mid] < target) {
-            return mid + 1
-        } else if (mid === 0 && nums[mid] > target) {
-            return 0
         } else if (nums[mid] > target) {
             r = mid - 1
         } else {
             l = mid + 1
         }
     }
+    
+    return l
 };
