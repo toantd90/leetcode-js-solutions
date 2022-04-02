d615ba0d96dc0a08e3ff6ac8f2337fadba370d5f
ToanTran <toantdfu@gmail.com>
2022-04-03 08:40:08 +0900

    Time: 120 ms (100.00%), Space: 44.3 MB (100.00%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -4,12 +4,11 @@
  */
 const triangularSum = nums => {
     let prev = nums
-    let arr = new Array(nums.length - 1)
+
     for (let i = nums.length - 1; i >= 1; i--) {
         for (let j = 0; j < i; j++) {
-            arr[j] = (prev[j] + prev[j + 1]) % 10
+            prev[j] = (prev[j] + prev[j + 1]) % 10
         }
-        prev = arr
     }
 
     return prev[0]
