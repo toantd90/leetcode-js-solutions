b4043bd91f0c179d481491d3eb34e12c674a2846
ToanTran <toantdfu@gmail.com>
2022-04-03 18:06:50 +0900

    Time: 88 ms (100.00%), Space: 44.5 MB (100.00%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.js
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.js
@@ -1,28 +1,17 @@
+const toMin = time => {
+    const [hour, minute] = time.split(':').map(Number)
+    
+    return hour * 60 + minute
+}
+
 /**
  * @param {string} current
  * @param {string} correct
  * @return {number}
  */
 const convertTime = (current, correct) => {
-    const [currentHour, currentMinute] = current.split(':').map(Number)
-    const [correctHour, correctMinute] = correct.split(':').map(Number)
+    const diff = toMin(correct) - toMin(current)
+    console.log(diff)
     
-    
-    let convert = correctHour - currentHour
-    let diff = correctMinute < currentMinute ? correctMinute + 60 - currentMinute : correctMinute - currentMinute
-    
-    while (diff >= 5) {
-        if (diff / 15 >= 1) {
-            convert += Math.floor(diff / 15)
-            diff %= 15
-            continue
-        }
-        
-        if (diff /= 5 >= 1) {
-            convert += Math.floor(diff / 5)
-            diff %= 5
-        }
-    }
-    
-    return convert += diff + (correctMinute < currentMinute ? -1 : 0)
+    return Math.floor(diff / 60) + Math.floor(diff % 60 / 15) + Math.floor(diff % 15 / 5) + diff % 5
 };
