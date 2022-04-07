e8bf1c6f1610e9b214b9b9b567415af017e54c0a
ToanTran <toantdfu@gmail.com>
2022-04-07 10:43:00 +0900

    Time: 132 ms (6.16%), Space: 42.8 MB (66.67%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+const lastStoneWeight = stones => {
+    const maxHeap = new MaxPriorityQueue()
+    
+    for (let weight of stones) {
+        maxHeap.enqueue(weight)
+    }
+    
+    while (maxHeap.size() > 1) {
+        const first = maxHeap.dequeue()
+        const second = maxHeap.dequeue()
+        
+        if (first.element !== second.element) {
+            maxHeap.enqueue(Math.abs(first.element - second.element))
+        }
+    }
+    
+    return maxHeap.size() === 0 ? 0 : maxHeap.dequeue().element
+};
