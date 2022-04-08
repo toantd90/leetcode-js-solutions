e2dcd7668bad024b35230d4c86ab4e2ac5566b8d
ToanTran <toantdfu@gmail.com>
2022-04-09 07:44:19 +0900

    Time: 68 ms (83.00%), Space: 44.4 MB (27.07%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -9,15 +9,13 @@ const countNegatives = grid => {
     let cnt = 0
     
     for (let i = 0; i < m; i++) {
-        let isFound = false
+        let tempR = r
         while (l <= r) {
             const mid = l + Math.floor((r - l) / 2)
             
             if (grid[i][mid] < 0 && (!grid[i][mid - 1] || grid[i][mid - 1] > 0)) {
-                isFound = true
                 r = mid - 1
                 cnt += n - mid
-                console.log(cnt)
                 break
             } else if (grid[i][mid] < 0) {
                 r = mid - 1
@@ -27,11 +25,9 @@ const countNegatives = grid => {
         }
         
         
-        if (!isFound) {
-            r = n - 1
-            cnt += n - l
-        }
+        r = tempR
         l = 0
+        cnt += n - r - 1
     }
     
     return cnt
