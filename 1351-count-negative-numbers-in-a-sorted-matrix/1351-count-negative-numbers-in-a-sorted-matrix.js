b355766cf333b075ac22b1b07da3ebd256acf82b
ToanTran <toantdfu@gmail.com>
2022-04-09 06:57:18 +0900

    Time: 91 ms (42.29%), Space: 45.3 MB (12.45%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -1,38 +1,38 @@
-const findFirstIndexOfNegative = nums => {
-    const n = nums.length
-    let l = 0
-    let r = n
-    
-    while (l <= r) {
-        const mid = l + Math.floor((r - l) / 2)
-        
-        if (nums[mid] >= 0) {
-            l = mid + 1
-        } else {
-            r = mid - 1
-        }
-    }
-    
-    return l
-} 
-
-const countNegativesInAnArray = nums => {
-    const firstIndex = findFirstIndexOfNegative(nums)
-    
-    return nums.length - firstIndex
-}
-
 /**
  * @param {number[][]} grid
  * @return {number}
  */
 const countNegatives = grid => {
     const m = grid.length
-    let res = 0
+    const n = grid[0].length
+    let l = 0, r = n - 1
+    let cnt = 0
     
     for (let i = 0; i < m; i++) {
-        res += countNegativesInAnArray(grid[i])
+        let isFound = false
+        while (l <= r) {
+            const mid = l + Math.floor((r - l) / 2)
+            
+            if (grid[i][mid] < 0 && (!grid[i][mid - 1] || grid[i][mid - 1] > 0)) {
+                isFound = true
+                r = mid - 1
+                cnt += n - mid
+                console.log(cnt)
+                break
+            } else if (grid[i][mid] < 0) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+        
+        
+        if (!isFound) {
+            r = n - 1
+            cnt += n - l
+        }
+        l = 0
     }
     
-    return res
+    return cnt
 };
