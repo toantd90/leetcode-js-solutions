3beec20351632821f9415aa40eff331b342b0e65
ToanTran <toantdfu@gmail.com>
2022-04-10 15:54:29 +0900

    Time: 109 ms (100.00%), Space: 42.2 MB (100.00%) - LeetHub

---
+++ 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} expression
+ * @return {string}
+ */
+const minimizeResult = expression => {
+    const [first, second] = expression.split('+')
+    let res = Number(first) + Number(second)
+    let ans = '(' + first + '+' + second + ')'
+    
+    if (expression.length === 3) return ans
+    for (let i = 0; i < first.length; i++) {
+        for (let j = 0; j < second.length; j++) {
+            const x = Number(first.substring(0, i + 1))
+            const a = Number(first.substring(i + 1))
+            const b = Number(second.substring(0, j + 1))
+            const y = Number(second.substring(j + 1))
+            
+            let cur
+                if (a === 0)
+                    cur = (x + b) * (y || 1)
+                else 
+                    cur = x * (a + b) * (y || 1)
+           
+            if (cur < res) {
+                res = cur
+                if (a === 0) {
+                    ans = '(' + x + '+' + b + ')' + (y !== 0 ? y : '') 
+                } else {
+                    ans = x + '(' + a + '+' + b + ')' + (y !== 0 ? y : '')     
+                }
+            }
+        }
+    }
+    
+    return ans
+};
