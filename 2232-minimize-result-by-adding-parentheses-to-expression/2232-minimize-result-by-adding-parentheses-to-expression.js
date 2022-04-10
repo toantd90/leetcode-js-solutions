5a3b2670b6e973768c4f939a691a4bb4aff18f46
ToanTran <toantdfu@gmail.com>
2022-04-11 06:02:59 +0900

    Time: 83 ms (100.00%), Space: 42.4 MB (100.00%) - LeetHub

--- 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.js
+++ 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.js
@@ -2,35 +2,34 @@
  * @param {string} expression
  * @return {string}
  */
-const minimizeResult = expression => {
-    const [first, second] = expression.split('+')
-    let res = Number(first) + Number(second)
-    let ans = '(' + first + '+' + second + ')'
-    
-    // if (expression.length === 3) return ans
-    for (let i = 0; i < first.length; i++) {
-        for (let j = 0; j < second.length; j++) {
-            const x = Number(first.substring(0, i + 1))
-            const a = Number(first.substring(i + 1))
-            const b = Number(second.substring(0, j + 1))
-            const y = Number(second.substring(j + 1))
-            
-            let cur
-            if (a === 0)
-                cur = (x + b) * (y || 1)
-            else 
-                cur = x * (a + b) * (y || 1)
-           
-            if (cur < res) {
-                res = cur
-                if (a === 0) {
-                    ans = '(' + x + '+' + b + ')' + (y !== 0 ? y : '') 
-                } else {
-                    ans = x + '(' + a + '+' + b + ')' + (y !== 0 ? y : '')     
-                }
-            }
+const minimizeResult = (expression) => {
+  const [first, second] = expression.split('+');
+  let res = Number(first) + Number(second);
+  let ans = '(' + first + '+' + second + ')';
+
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      const x = Number(first.substring(0, i + 1));
+      const a = Number(first.substring(i + 1));
+      const b = Number(second.substring(0, j + 1));
+      const y = Number(second.substring(j + 1));
+
+      let cur;
+      if (a === 0) {
+        cur = (x + b) * (y || 1);
+        if (cur < res) {
+          res = cur;
+          ans = '(' + x + '+' + b + ')' + (y !== 0 ? y : '');
         }
+      } else {
+        cur = x * (a + b) * (y || 1);
+        if (cur < res) {
+          res = cur;
+          ans = x + '(' + a + '+' + b + ')' + (y !== 0 ? y : '');
+        }
+      }
     }
-    
-    return ans
+  }
+
+  return ans;
 };
