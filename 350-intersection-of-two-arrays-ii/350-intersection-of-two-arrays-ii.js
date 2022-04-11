d5db48407de742666c8a603dd912b7e03b2d7746
ToanTran <toantdfu@gmail.com>
2022-04-12 08:17:28 +0900

    Time: 64 ms (93.32%), Space: 44.7 MB (22.50%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -4,22 +4,18 @@
  * @return {number[]}
  */
 const intersect = (nums1, nums2) => {
-    let map1 = {}, map2 = {}
+    let map1 = {}
     
     for (let num of nums1) {
         map1[num] = (map1[num] || 0) + 1
     }
     
-    for (let num of nums2) {
-        map2[num] = (map2[num] || 0) + 1
-    }
-    
     let ans = []
     
-    for (let num in map1) {
-        if (map2[num]) {
-            const concatArr = new Array(Math.min(map1[num], map2[num])).fill(num)
-            ans = [...ans, ...concatArr]
+    for (let num of nums2) {
+        if (map1[num]) {
+            map1[num]--
+            ans.push(num)
         }
     }
     
