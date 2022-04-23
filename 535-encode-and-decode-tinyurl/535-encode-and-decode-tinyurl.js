881aea1f0b0c137b08a63613860da04239630cc3
ToanTran <toantdfu@gmail.com>
2022-04-24 05:46:48 +0900

    Time: 100 ms (43.27%), Space: 44.3 MB (25.00%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.js
@@ -0,0 +1,40 @@
+let codeDb = new Map(), urlDb = new Map()
+const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const BASE_URL = 'http://tinyurl.com/'
+
+
+const getCode = () => {
+    return new Array(6).fill().map(_ => chars.charAt(Math.floor(Math.random() * 62))).join('')
+}
+
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+const encode = longUrl => {
+    let code = getCode()
+    
+    while (codeDb.has(code)) code = getCode()
+    codeDb.set(code, longUrl)
+    
+    const shortUrl = BASE_URL + code
+    urlDb.set(shortUrl, longUrl)
+    
+    return shortUrl
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+const decode = shortUrl => urlDb.get(shortUrl)
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
