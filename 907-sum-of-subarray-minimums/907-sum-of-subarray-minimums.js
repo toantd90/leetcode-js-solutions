db12f6e47fe6adce10d76bf3b773658d6fdf231d
ToanTran <toantdfu@gmail.com>
2022-05-05 08:52:58 +0900

    Time: 112 ms (81.75%), Space: 51.9 MB (46.83%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.js
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.js
@@ -3,17 +3,21 @@
  * @return {number}
  */
 const sumSubarrayMins = arr => {
-    let minimums = 0
-    const len = arr.length
+    let dp = []
+    let stack = [-1]
+    let result = 0
+    const MOD = 1e9 + 7
     
-    for (let i = 0; i < len; i++) {
-        let min = arr[i]
-       
-        for (let j = i; j < len; j++) {
-            min = Math.min(min, arr[j])
-            minimums += min
+    for (let i = 0; i < arr.length; i++) {
+        while (stack[stack.length - 1] !== -1 && arr[i] < arr[stack[stack.length - 1]]) {
+            stack.pop()
         }
+        const j = stack[stack.length - 1]
+        
+        dp[i] = (dp[j] || 0) + arr[i] * (i - j)
+        result += dp[i]
+        stack.push(i)
     }
     
-    return minimums % (1e9 + 7)
+    return result % MOD
 };
