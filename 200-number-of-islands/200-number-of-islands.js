7c0aefd825d98f4994dae4da157857a4c46077d3
ToanTran <toantdfu@gmail.com>
2022-05-06 06:43:06 +0900

    Time: 85 ms (85.55%), Space: 44.7 MB (90.33%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -1,52 +1,33 @@
-const isInGrid = (row, col, rows, cols) => row >= 0 && row < rows && col >= 0 && col < cols
-
-const bfs = (grid, [row, col]) => {
-    const rows = grid.length
-    const cols = grid[0].length
-    const dirs = [0, 1, 0, -1, 0]
-    
-    let queue = [[row, col]]
-    grid[row][col] = -1
-    
-    while (queue.length) {
-        const size = queue.length;
-        
-        for (let i = 0; i < size; i++) {
-            const [row, col] = queue.shift()
-        
-            for (let j = 0; j < 4; j++) {
-               const nextRow = row + dirs[j]
-               const nextCol = col + dirs[j + 1]
-
-               if (isInGrid(nextRow, nextCol, rows, cols) && grid[nextRow][nextCol] === '1') {
-                   grid[nextRow][nextCol] = '-1'
-                   queue.push([nextRow, nextCol])
-               }
-            }
-        }
-    }
-}
-
 /**
  * @param {character[][]} grid
  * @return {number}
  */
-const numIslands = grid => {
-    const rows = grid.length
-    const cols = grid[0].length
-    
-    let islands = 0
-    
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < cols; col++) {
-            if (grid[row][col] === '0' || grid[row][col] === '-1') {
-                continue
-            }
-            
-            bfs(grid, [row, col])
-            islands++
-        }
+
+const dfs = (grid, i, j) => {
+    if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1 || grid[i][j] === '0') {
+      return 0;
     }
     
-    return islands
+    grid[i][j] = '0';
+    dfs(grid, i-1, j);
+    dfs(grid, i+1, j);
+    dfs(grid, i, j-1);
+    dfs(grid, i, j+1);
+    return 1;
+}
+
+const numIslands = function(grid) {
+  if (!grid || grid.length === 0) return 0;
+  
+  let numIslands = 0;
+  
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '1') {
+        numIslands += dfs(grid, i, j);
+      }
+    }
+  }
+  
+  return numIslands;
 };
