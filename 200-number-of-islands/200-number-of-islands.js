c6333ec0dff729b6e80dd4e6f44cdca8b7594d47
ToanTran <toantdfu@gmail.com>
2022-05-06 06:40:58 +0900

    Time: 86 ms (84.67%), Space: 48.5 MB (42.33%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -1,44 +1,52 @@
+const isInGrid = (row, col, rows, cols) => row >= 0 && row < rows && col >= 0 && col < cols
+
+const bfs = (grid, [row, col]) => {
+    const rows = grid.length
+    const cols = grid[0].length
+    const dirs = [0, 1, 0, -1, 0]
+    
+    let queue = [[row, col]]
+    grid[row][col] = -1
+    
+    while (queue.length) {
+        const size = queue.length;
+        
+        for (let i = 0; i < size; i++) {
+            const [row, col] = queue.shift()
+        
+            for (let j = 0; j < 4; j++) {
+               const nextRow = row + dirs[j]
+               const nextCol = col + dirs[j + 1]
+
+               if (isInGrid(nextRow, nextCol, rows, cols) && grid[nextRow][nextCol] === '1') {
+                   grid[nextRow][nextCol] = '-1'
+                   queue.push([nextRow, nextCol])
+               }
+            }
+        }
+    }
+}
+
 /**
  * @param {character[][]} grid
  * @return {number}
  */
-const directions = [
-  [0, 1],
-  [0, -1],
-  [1, 0],
-  [-1, 0],
-];
-const isInRange = (x, y, maxX, maxY) =>
-  x >= 0 && x < maxX && y >= 0 && y < maxY;
-const bfs = ([i, j], grid, m, n) => {
-  let queue = [[i, j]];
-  grid[i][j] = '0';
-  while (queue.length) {
-    const [x, y] = queue.shift();
-
-    for (let [addX, addY] of directions) {
-      const newX = x + addX;
-      const newY = y + addY;
-      if (isInRange(newX, newY, m, n) && grid[newX][newY] === '1') {
-        queue.push([newX, newY]);
-        grid[newX][newY] = '0';
-      }
+const numIslands = grid => {
+    const rows = grid.length
+    const cols = grid[0].length
+    
+    let islands = 0
+    
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (grid[row][col] === '0' || grid[row][col] === '-1') {
+                continue
+            }
+            
+            bfs(grid, [row, col])
+            islands++
+        }
     }
-  }
-};
-const numIslands = (grid) => {
-  const m = grid.length;
-  const n = grid[0].length;
-  let islands = 0;
-
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (grid[i][j] === '1') {
-        bfs([i, j], grid, m, n);
-        islands++;
-      }
-    }
-  }
-
-  return islands;
+    
+    return islands
 };
