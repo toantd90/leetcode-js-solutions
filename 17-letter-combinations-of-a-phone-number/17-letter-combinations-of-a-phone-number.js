1ce757cce1670f6560ae4f97c28c8ac950b5d30f
ToanTran <toantdfu@gmail.com>
2022-05-09 10:27:11 +0900

    Time: 106 ms (10.15%), Space: 42 MB (62.05%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -1,42 +1,34 @@
-const map = {
-    2: ['a', 'b', 'c'],
-    3: ['d', 'e', 'f'],
-    4: ['g', 'h', 'i'],
-    5: ['j', 'k', 'l'],
-    6: ['m', 'n', 'o'],
-    7: ['p', 'q', 'r', 's'],
-    8: ['t', 'u', 'v'],
-    9: ['w', 'x', 'y', 'z'],
-}
-
-
-
-
 /**
  * @param {string} digits
  * @return {string[]}
  */
 const letterCombinations = digits => {
-    if (digits.length === 0) return []
-    
-    let res = []
-    
-    const backtrack = (list = []) => {
-        if (list.length === digits.length) {
-            res.push(list.join(''))
-        } else {
-            const index = list.length
-            const digit = digits[index]
-            const chars = map[digit]
-            for (let i = 0; i < chars.length; i++) {
-                list.push(chars[i])
-                backtrack(list)
-                list.pop()
-            }
-        }
+    const digitMap = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w','x', 'y', 'z'],
     }
     
-    backtrack()
+    let result = []
+    for (let digit of digits) {
+       if (!result.length) {
+           result = [...digitMap[digit]]
+       } else {
+           const size = result.length
+           for (let i = 0; i < size; i++) {
+               for (let letter of digitMap[digit]) {
+                   result.push(result[i] + letter)
+               }
+           }
+           
+           result.splice(0, size)
+       }
+    }
     
-    return res
+    return result
 };
