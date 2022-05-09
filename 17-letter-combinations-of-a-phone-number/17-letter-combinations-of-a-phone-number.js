cca6c017efce59c261c6d27d5718f178055b9d8c
ToanTran <toantdfu@gmail.com>
2022-05-09 10:35:39 +0900

    Time: 71 ms (60.33%), Space: 42.3 MB (38.54%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -1,34 +1,32 @@
+const digitMap = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w','x', 'y', 'z'],
+}
+
 /**
  * @param {string} digits
  * @return {string[]}
  */
 const letterCombinations = digits => {
-    const digitMap = {
-        2: ['a', 'b', 'c'],
-        3: ['d', 'e', 'f'],
-        4: ['g', 'h', 'i'],
-        5: ['j', 'k', 'l'],
-        6: ['m', 'n', 'o'],
-        7: ['p', 'q', 'r', 's'],
-        8: ['t', 'u', 'v'],
-        9: ['w','x', 'y', 'z'],
-    }
-    
+    if (digits.length === 0) return []
     let result = []
-    for (let digit of digits) {
-       if (!result.length) {
-           result = [...digitMap[digit]]
-       } else {
-           const size = result.length
-           for (let i = 0; i < size; i++) {
-               for (let letter of digitMap[digit]) {
-                   result.push(result[i] + letter)
-               }
-           }
-           
-           result.splice(0, size)
-       }
-    }
-    
+    dfs(digits, result, '')
+        
     return result
 };
+
+const dfs = (digits, result, cur) => {
+    if (cur.length === digits.length) {
+        result.push(cur)
+        return
+    }
+    for (let letter of digitMap[digits[cur.length]]) {
+        dfs(digits, result, cur + letter)
+    }
+}
