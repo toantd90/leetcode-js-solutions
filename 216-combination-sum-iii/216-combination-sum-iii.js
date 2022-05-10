cdc6f48fe9deac17d5d3d2ab3741688fdd1da5eb
ToanTran <toantdfu@gmail.com>
2022-05-10 12:41:32 +0900

    Time: 67 ms (76.40%), Space: 41.8 MB (90.02%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.js
+++ 216-combination-sum-iii/216-combination-sum-iii.js
@@ -5,19 +5,21 @@
  */
 const combinationSum3 = (k, n) => {
     let result = []
-    const backtrack = (list, sum, start) => {
+    const combination = (list, sum, start) => {
         if (sum > n) return
         if (sum === n && list.length === k) {
-            result.push(list)
+            result.push([...list])
             return
         }
 
         for (let i = start; i <= 9; i++) {
-            backtrack([...list, i], sum + i, i + 1)
+            list.push(i)
+            combination(list, sum + i, i + 1)
+            list.pop()
         }
     }
     
-    backtrack([], 0, 1)
+    combination([], 0, 1)
     
     return result
 };
