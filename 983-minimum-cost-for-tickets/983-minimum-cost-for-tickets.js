a1795d7f8da57aa24806b10712e32e1e53cd9901
ToanTran <toantdfu@gmail.com>
2022-05-24 06:51:55 +0900

    Time: 102 ms (30.95%), Space: 42.3 MB (80.95%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} days
+ * @param {number[]} costs
+ * @return {number}
+ */
+const mincostTickets = (days, costs) => {
+    const firstDay = days[0]
+    const lastDay = days[days.length - 1]
+    let dp = new Array(lastDay + 1).fill(0)
+    
+    for (let i = 0; i < days.length; i++) {
+        const currentDay = days[i]
+        for (let j = days[i - 1]; j < currentDay; j++) {
+            dp[j] = dp[days[i - 1]]
+        }
+        
+        dp[currentDay] = Math.min(dp[currentDay - 1] + costs[0], (dp[currentDay - 7] || 0) + costs[1], (dp[currentDay - 30] || 0) + costs[2])
+    }
+    
+    
+    return dp[lastDay]
+};
