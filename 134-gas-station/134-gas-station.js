5bbdfede77b23133b25eb98b86b80bcd1abaa206
ToanTran <toantdfu@gmail.com>
2022-05-28 05:56:49 +0900

    Time: 102 ms (56.60%), Space: 50.5 MB (81.13%) - LeetHub

---
+++ 134-gas-station/134-gas-station.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    const n = gas.length
+    let start = 0
+    let totalDiff = 0
+    let curDiff = 0
+    
+    
+    for (let i = 0; i < n; i++) {
+        totalDiff += gas[i] - cost[i]
+        curDiff += gas[i] - cost[i]
+        
+        if (curDiff < 0) {
+            curDiff = 0
+            start = i + 1
+        }
+    }
+
+    return totalDiff < 0 ? -1 : start
+};
