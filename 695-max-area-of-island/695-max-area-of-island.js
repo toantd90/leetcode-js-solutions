3352f68a4dd1248e6f2afab873e45c137db7f081
ToanTran <toantdfu@gmail.com>
2022-05-31 05:35:54 +0900

    Time: 107 ms (58.63%), Space: 44.9 MB (78.35%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -8,33 +8,17 @@ const maxAreaOfIsland = grid => {
     const n = grid[0].length
     
     const isInGrid = (row, col) => row >= 0 && row < m && col >= 0 && col < n
-    const dirs = [0, 1, 0, -1, 0]
-    
     
     const dfs = (row, col) => {
-        let area = 1
-        let stack = [[row, col]]
-        while (stack.length) {
-            const [row, col] = stack.shift()
-
-            for (let i = 0; i < 4; i++) {
-                const nextRow = row + dirs[i]
-                const nextCol = col + dirs[i + 1]
-                if (isInGrid(nextRow, nextCol) && grid[nextRow][nextCol]) {
-                    grid[nextRow][nextCol] = 0
-                    area++
-                    stack.push([nextRow, nextCol])
-                }
-            }
-        }
+        if (!isInGrid(row, col) || !grid[row][col]) return 0
+        grid[row][col] = 0
         
-        return area
+        return 1 + dfs(row + 1, col) + dfs(row - 1, col) + dfs(row, col + 1) + dfs(row, col - 1)
     }
     
     for (let row = 0; row < m; row++) {
         for (let col = 0; col < n; col++) {
             if (grid[row][col]) {
-                grid[row][col] = 0
                 maxArea = Math.max(maxArea, dfs(row, col))
             }
         }
