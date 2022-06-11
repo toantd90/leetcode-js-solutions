7e8b417afc2922d559610ce5251b414f26fb1c05
ToanTran <toantdfu@gmail.com>
2022-06-12 06:46:18 +0900

    Time: 285 ms (27.66%), Space: 80.8 MB (34.04%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
@@ -3,29 +3,31 @@
  * @param {number} x
  * @return {number}
  */
-const minOperations = function(nums, x) {
-    let left = { 0: -1 }
+var minOperations = function(nums, x) {
+    let left = {}
+    
+    const n = nums.length
+    
     let sum = 0
-    let ans = nums.length + 1
-    for (let i = 0; i < nums.length; i++) {
+    let ans = n + 1
+    
+    for (let i = 0; i < n; i++) {
         sum += nums[i]
-        if (sum === x)
-            ans = Math.min(ans, i + 1)
-        if (sum > x)
-            break
-        left[sum] = i
+
+        if (sum > x) break
+        if (sum === x) ans = i + 1
+        
+        left[x - sum] = i + 1
     }
     
     sum = 0
-    
-    for (let i = nums.length - 1; i >= 0; i--) {
+    for (let i = n - 1; i >= 0; i--) {
         sum += nums[i]
-        if (sum > x)
-            break
-        if (left[x - sum] !== undefined) {
-            ans = Math.min(ans, left[x - sum] + 1 + nums.length - i)
-        }
+        
+        if (sum > x) break
+        if (sum === x) ans = Math.min(ans, n - i)       
+        if (left[sum]) ans = Math.min(ans, n - i + left[sum])
     }
     
-    return ans === nums.length + 1 ? -1 : ans
+    return ans === n + 1 ? -1 : ans
 };
