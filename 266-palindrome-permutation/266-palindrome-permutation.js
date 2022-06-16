7bc064f06da59c36cfae0cd7d6bab8e0260a7ce2
ToanTran <toantdfu@gmail.com>
2022-06-17 06:39:45 +0900

    Time: 64 ms (83.43%), Space: 41.5 MB (96.69%) - LeetHub

---
+++ 266-palindrome-permutation/266-palindrome-permutation.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var canPermutePalindrome = function(s) {
+    let charCount = {}
+    
+    for (let c of s) {
+        charCount[c] = (charCount[c] || 0) + 1
+    }
+    
+    let odd = 0
+    let values = Object.values(charCount)
+    console.log(values)
+    
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] % 2 === 1) odd++
+        if (odd > 1) return false
+    }
+        
+    return true
+};
