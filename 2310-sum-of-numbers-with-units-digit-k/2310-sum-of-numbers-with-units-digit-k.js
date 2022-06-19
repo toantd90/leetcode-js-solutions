e725e885804269c2efcba08d18ba33191697601e
ToanTran <toantdfu@gmail.com>
2022-06-20 04:32:25 +0900

    Time: 93 ms (100.00%), Space: 41.8 MB (100.00%) - LeetHub

---
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} num
+ * @param {number} k
+ * @return {number}
+ */
+var minimumNumbers = function(num, k) {
+    if (num === 0) return 0
+    if (num === k) return 1
+    if (k === 0) return num % 10 !== 0 ? -1 : 1
+    
+    let x = Math.floor(num / k)
+    
+    while (x >= 0) {
+        if ((num - 10 * x) > 0 && (num - 10 * x) % k === 0) {
+            return (num - 10 * x) / k
+        }
+        x--
+    }
+    
+    return -1
+};
