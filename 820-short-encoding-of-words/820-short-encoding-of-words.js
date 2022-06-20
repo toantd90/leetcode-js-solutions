5adefaca4b9e9797dacfc6c9d4424745b61a5771
ToanTran <toantdfu@gmail.com>
2022-06-21 06:03:30 +0900

    Time: 406 ms (35.71%), Space: 70.5 MB (7.14%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.js
+++ 820-short-encoding-of-words/820-short-encoding-of-words.js
@@ -54,26 +54,6 @@ const Trie = function() {
     }
   };
   
-  // check if it contains a whole word.
-  this.contains = function(word) {
-    let node = this.root;
-
-    // for every character in the word
-    for(let i = 0; i < word.length; i++) {
-      // check to see if character node exists in children.
-      if (node.children[word[i]]) {
-        // if it exists, proceed to the next depth of the trie.
-        node = node.children[word[i]];
-      } else {
-        // doesn't exist, return false since it's not a valid word.
-        return false;
-      }
-    }
-
-    // we finished going through all the words, but is it a whole word?
-    return node.end;
-  };
-  
   // returns every word with given prefix
   this.find = function(prefix) {
     let node = this.root;
@@ -108,45 +88,6 @@ const Trie = function() {
       findAllWords(node.children[child], arr);
     }
   }
-
-  // removes a word from the trie.
-  this.remove = function (word) {
-      let root = this.root;
-
-      if(!word) return;
-
-      // recursively finds and removes a word
-      const removeWord = (node, word) => {
-
-          // check if current node contains the word
-          if (node.end && node.getWord() === word) {
-
-              // check and see if node has children
-              let hasChildren = Object.keys(node.children).length > 0;
-
-              // if has children we only want to un-flag the end node that marks the end of a word.
-              // this way we do not remove words that contain/include supplied word
-              if (hasChildren) {
-                  node.end = false;
-              } else {
-                  // remove word by getting parent and setting children to empty dictionary
-                  node.parent.children = {};
-              }
-
-              return true;
-          }
-
-          // recursively remove word from all children
-          for (let key in node.children) {
-              removeWord(node.children[key], word)
-          }
-
-          return false
-      };
-
-      // call remove word on root node
-      removeWord(root, word);
-  };
 }
 
 /**
