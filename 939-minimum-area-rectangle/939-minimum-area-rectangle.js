c8e96b67cd06ef9c8dd234dff1aa405732a621b2
ToanTran <toantdfu@gmail.com>
2022-07-01 06:48:17 +0900

    Time: 424 ms (76.39%), Space: 49.8 MB (61.57%) - LeetHub

--- 939-minimum-area-rectangle/939-minimum-area-rectangle.js
+++ 939-minimum-area-rectangle/939-minimum-area-rectangle.js
@@ -4,18 +4,24 @@
  */
 var minAreaRect = function(points) {
   let area = Number.POSITIVE_INFINITY
-    
-  let seen = new Set()
+  let pointMap = {}  
   
-  for (let [x1, y1] of points) {
-    for (let p of seen) {
-      const [x2, y2]  = p.split('-')
-      if (seen.has(x1 + '-' + y2) && seen.has(x2 + '-' + y1)) {
+  for (let [x, y] of points) {
+    if (!pointMap[x]) pointMap[x] = new Set()
+    
+    pointMap[x].add(y)
+  }
+  
+  for (let i = 0; i < points.length; i++) {
+    const [x1, y1] = points[i]
+    for (let j = i + 1; j < points.length; j++) {
+      const [x2, y2] = points[j]
+      if (x1 === x2 || y1 === y2) continue
+      
+      if (pointMap[x1].has(y2) && pointMap[x2].has(y1)) {
         area = Math.min(area, Math.abs(x1 - x2) * Math.abs(y1 - y2))
       }
     }
-    
-    seen.add(x1 + '-' + y1)
   }
   
   return area === Number.POSITIVE_INFINITY ? 0 : area
