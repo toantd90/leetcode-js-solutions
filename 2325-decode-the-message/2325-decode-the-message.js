d13807e18f0a776e9efcd28afb4bcdc499b9b726
ToanTran <toantdfu@gmail.com>
2022-07-04 06:49:54 +0900

    Time: 85 ms (100.00%), Space: 45 MB (100.00%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.js
+++ 2325-decode-the-message/2325-decode-the-message.js
@@ -4,26 +4,35 @@
  * @return {string}
  */
 const decodeMessage = (key, message) => {
-  let substitutionTable = {}
+  // Substitution is like a map for decoded character
+  // If we find a character in message what would it transform to after decode
+
+  // Initialize the substitution table 
+  // Spaces ' ' are transformed to themselves
+  let substitutionTable = {' ': ' '}
   
+  // Each character maps to '' (empty string) at first
   for (let i = 0; i < 26; i++) {
     substitutionTable[String.fromCharCode(97 + i)] = ''
   }
   
-  let i = 0
-  let j = 0
-  while (j < 26) {
-    if (!substitutionTable[key[i]] && key[i] !== ' ') {
-      substitutionTable[key[i]] = String.fromCharCode(97 + j)
-      j++  
+  // Map each character with decoded character
+  // Finished when we map all the character
+  // Timespace complexity: O(n) with n is length of key
+  let keyIndex = 0
+  let mapIndex = 0
+  while (mapIndex < 26) {
+    if (!substitutionTable[key[keyIndex]] && key[keyIndex] !== ' ') {
+      substitutionTable[key[keyIndex]] = String.fromCharCode(97 + mapIndex)
+      mapIndex++  
     }
-    i++
+    keyIndex++
   }
-  
-  substitutionTable[' '] = ' '
   
   let ans = ''
   
+  // Iterate over the message to decode message
+  // For every single character assign it to decoded character from substitution table
   for (let i = 0; i < message.length; i++) {
     ans += substitutionTable[message[i]]
   }
