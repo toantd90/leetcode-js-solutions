3866b35e7f787cfa43c7c42a42fb0fc8b0b8be3a
ToanTran <toantdfu@gmail.com>
2022-07-04 06:39:52 +0900

    Time: 84 ms (100.00%), Space: 45.4 MB (100.00%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} key
+ * @param {string} message
+ * @return {string}
+ */
+var decodeMessage = function(key, message) {
+  let substitutionTable = {}
+  
+  for (let i = 0; i < 26; i++) {
+    substitutionTable[String.fromCharCode(97 + i)] = ''
+  }
+  
+  let i = 0
+  let j = 0
+  while (j < 26) {
+    if (!substitutionTable[key[i]] && key[i] !== ' ') {
+      substitutionTable[key[i]] = String.fromCharCode(97 + j)
+      j++  
+    }
+    i++
+  }
+  
+  substitutionTable[' '] = ' '
+  
+  let ans = ''
+  
+  for (let i = 0; i < message.length; i++) {
+    ans += substitutionTable[message[i]]
+  }
+  
+  return ans
+};
