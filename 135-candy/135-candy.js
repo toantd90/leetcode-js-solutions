2d14df34300b175bd15a3e99512d2a049851d335
ToanTran <toantdfu@gmail.com>
2022-07-05 05:46:30 +0900

    Time: 119 ms (35.26%), Space: 45.5 MB (64.74%) - LeetHub

--- 135-candy/135-candy.js
+++ 135-candy/135-candy.js
@@ -4,32 +4,25 @@
  */
 var candy = function(ratings) {
   const children = ratings.length
-  let leftToRight = new Array(children)
-  leftToRight[0] = 1
-  let rightToLeft = new Array(children)
-  rightToLeft[children - 1] = 1
+  let candies = new Array(children).fill(1)
   
   for (let i = 1; i < children; i++) {
     if (ratings[i] > ratings[i - 1]) {
-      leftToRight[i] = leftToRight[i - 1] + 1
+      candies[i] = candies[i - 1] + 1
     } else {
-      leftToRight[i] = 1
+      candies[i] = 1
     }
   }
   
-  for (let i = children - 1; i >= 0; i--) {
+  let sum = candies[children - 1]
+  
+  for (let i = children - 2; i >= 0; i--) {
     if (ratings[i] > ratings[i + 1]) {
-      rightToLeft[i] = rightToLeft[i + 1] + 1
-    } else {
-      rightToLeft[i] = 1
+      candies[i] = Math.max(candies[i], candies[i + 1] + 1)
     }
+    
+    sum += candies[i]
   }
   
-  let candies = 0
-  
-  for (let i = 0; i < children; i++) {
-    candies += Math.max(leftToRight[i], rightToLeft[i])
-  }
-  
-  return candies
+  return sum
 };
