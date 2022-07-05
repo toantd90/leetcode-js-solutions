962285e649f4a39ba9be97ae1895619d9177ea59
ToanTran <toantdfu@gmail.com>
2022-07-06 05:40:08 +0900

    Time: 110 ms (24.44%), Space: 42.7 MB (55.76%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.js
+++ 419-battleships-in-a-board/419-battleships-in-a-board.js
@@ -2,7 +2,7 @@
  * @param {character[][]} board
  * @return {number}
  */
-var countBattleships = function(board) {
+const countBattleships = board => {
   let cnt = 0
     
   for (let i = 0; i < board.length; i++) {
