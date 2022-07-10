ccf7a18b12e5c123687b70cb0d51da1d1a05fb99
Toan Tran <toantdfu@gmail.com>
2022-07-11 06:32:37 +0900

    Time: 128 ms (41.57%), Space: 48.3 MB (43.82%) - LeetHub

--- 265-paint-house-ii/265-paint-house-ii.js
+++ 265-paint-house-ii/265-paint-house-ii.js
@@ -2,16 +2,13 @@
  * @param {number[][]} costs
  * @return {number}
  */
-var minCostII = function(costs) {
+const minCostII = costs => {
   const k = costs[0].length
   
   for (let i = 1; i < costs.length; i++) {
-    let curCost = [...costs[i]]
     for (let j = 0; j < k; j++) {
-      curCost[j] += Math.min(...costs[i - 1].slice(0, j).concat(costs[i - 1].slice(j + 1)))
+      costs[i][j] += Math.min(...costs[i - 1].slice(0, j).concat(costs[i - 1].slice(j + 1)))
     }
-    
-    costs[i] = curCost
   }
   
   return Math.min(...costs.pop())
