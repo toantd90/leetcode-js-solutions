6916e4e198860d078e2f487cb53dcbd0f31ac4de
Toan Tran <toantdfu@gmail.com>
2022-07-12 06:10:06 +0900

    Time: 56 ms (98.10%), Space: 42 MB (46.48%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -3,11 +3,16 @@
  * @return {number}
  */
 var rob = function(nums) {
-  let dp = [nums[0], Math.max(nums[0], nums[1])]
+  if (!nums || !nums.length) return 0
   
-  for (let i = 2; i < nums.length; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[[i]])
+  let prev1 = nums[0]
+  let prev2 = 0
+  
+  for (let i = 1; i < nums.length; i++) {
+    const temp = prev1
+    prev1 = Math.max(prev2 + nums[i], prev1)
+    prev2 = temp
   }
   
-  return dp[nums.length - 1]
+  return prev1;
 };
