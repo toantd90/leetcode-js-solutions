3c2268ac97aaab9fd15be0e85d1545b5087e4ff4
Toan Tran <toantdfu@gmail.com>
2022-07-14 06:11:32 +0900

    Time: 70 ms (80.25%), Space: 41.8 MB (82.36%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.js
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -2,19 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
+const rob = nums => {
   const n = nums.length
   if (n == 0) return 0
   if (n == 1) return nums[0]
   if (n == 2) return Math.max(nums[0], nums[1])
 
-  let dp1 = [0, nums[0]]
-  let dp2 = [0, nums[1]]
+  let f1 = nums[0], f2 = 0
+  let s1 = nums[1], s2 = 0
   
   for (let i = 2; i < n; i++) {
-    dp1[i] = Math.max(dp1[i - 2] + nums[i - 1], dp1[i - 1])
-    dp2[i] = Math.max(dp2[i - 2] + nums[i], dp2[i - 1])
+    const t1 = f1
+    f1 = Math.max(f2 + nums[i - 1], f1)
+    f2 = t1
+    const t2 = s1
+    s1 = Math.max(s2 + nums[i], s1)
+    s2 = t2
   }
   
-  return Math.max(dp1[n - 1], dp2[n - 1])
+  return Math.max(f1, s1)
 };
