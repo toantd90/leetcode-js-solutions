451510152227bd6ace4dcc4a19bcc7178c2e76f6
Toan Tran <toantdfu@gmail.com>
2022-07-19 07:57:27 +0900

    Time: 63 ms (94.74%), Space: 41.8 MB (75.00%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.js
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.js
@@ -3,30 +3,12 @@
  * @param {string} word
  * @return {number}
  */
-var maxRepeating = function(s, w) {
-  if (s.length < w.length) return 0
-  let max = 0
-  for (let i = 0; i < s.length; i++) {
-    let sI = i
-    let wI = 0
-    let isSubstring = false
-    while (s[sI] == w[wI]) {
-      sI++
-      wI++
-      
-      if (wI == w.length) {
-        isSubstring = true
-        wI = 0
-      }
-    }
-    if (isSubstring && sI > i) {
-      max = Math.max(max, Math.floor((sI - i) / w.length))
-    }
-  }
+const maxRepeating = (s, w) => {
+	let result = 0;
+
+	while (s.includes(w.repeat(result + 1))) {
+		result += 1;
+	};
   
-  return max
+	return result;
 };
-
-
-"aaabaaaabaaabaaaabaaaabaaaabaaaaba"
-"aaaba"
