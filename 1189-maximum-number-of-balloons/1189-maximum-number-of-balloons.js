28046f1106b8fb5fe12169e6ac5c325280709159
Toan Tran <toantdfu@gmail.com>
2022-07-21 07:57:53 +0900

    Time: 77 ms (83.74%), Space: 43.4 MB (61.25%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+  let map = { b: 0, a: 0, l: 0, o: 0, n: 0 }
+  
+  for (let c of text) {
+    if (map[c] != undefined) {
+      map[c]++
+    }
+  }
+  
+  return Math.min(map['b'], map['a'], Math.floor(map['l'] / 2), Math.floor(map['o'] / 2), map['n'])
+};
