6ec6f5d2f679068f6e4071f06737c19116909738
Toan Tran <toantdfu@gmail.com>
2022-07-22 07:19:26 +0900

    Time: 74 ms (67.83%), Space: 42.6 MB (11.19%) - LeetHub

---
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function(nums) {
+  let maxSum = 0
+  let curSum = 0
+  
+  for (let i = 0; i < nums.length; i++) {
+    let j = i
+    let curSum = nums[j]
+    if (nums[j + 1] > nums[j]) {
+      while (nums[j + 1] > nums[j]) {
+        curSum += nums[j + 1]
+        j++
+      }
+    }
+    maxSum = Math.max(maxSum, curSum)
+  }
+  
+  return maxSum
+};
