8be2bc6d67a89f35e6a83a3d876ed960f34baaf9
Toan Tran <toantdfu@gmail.com>
2022-07-24 01:07:27 +0900

    Time: 905 ms (100.00%), Space: 110.3 MB (100.00%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.js
@@ -0,0 +1,34 @@
+class NumberContainers {
+  constructor() {
+    this.nums = {};
+    this.numMap = {};
+  }
+
+  change(index, number) {
+    this.nums[index] = number;
+
+    if (!this.numMap[number]) {
+      this.numMap[number] = new MinPriorityQueue();
+    }
+
+    this.numMap[number].enqueue(index);
+  }
+
+  find(number) {
+    if (this.numMap[number] && this.numMap[number].size() > 0) {
+      while (
+        this.numMap[number] &&
+        this.numMap[number].size() > 0 &&
+        this.nums[this.numMap[number].front().element] !== number
+      ) {
+        this.numMap[number].dequeue();
+      }
+
+      return this.numMap[number] && this.numMap[number].size() > 0
+        ? this.numMap[number].front().element
+        : -1;
+    } else {
+      return -1;
+    }
+  }
+}
