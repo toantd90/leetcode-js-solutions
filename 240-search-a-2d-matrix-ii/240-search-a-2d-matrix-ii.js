2bb1e3ecc3fbcb8dcee59706084162f82a38f6bd
Toan Tran <toantdfu@gmail.com>
2022-07-25 07:29:49 +0900

    Time: 1946 ms (41.46%), Space: 44.9 MB (70.84%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
@@ -1,37 +1,32 @@
-const binarySearch = (arr, target) => {
-  let l = 0
-  let r = arr.length - 1
-  
-  if (target < arr[l] || target > arr[r]) return
-  
-  while (l <= r) {
-    const mid = l + Math.floor((r - l) / 2)
-    
-    if (arr[mid] === target) {
-      return true
-    } else if (arr[mid] > target) {
-      r = mid - 1
-    } else {
-      l = mid + 1
-    }
-  }
-}
-
-
 /**
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
  */
 const searchMatrix = (matrix, target) => {
-  const m = matrix.length
-  const n = matrix[0].length
+  if (!matrix || matrix.length === 0) return false;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
   
-  for (let i = 0; i < m; i++) {
-    if (binarySearch(matrix[i], target)) {
-      return true
+  for (let i = 0; i < rows; i++) {
+    if (target < matrix[i][0] || target > matrix[i][cols - 1]) {
+      continue;
+    }
+    let left = 0;
+    let right = cols - 1;
+    
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (matrix[i][mid] === target) {
+        return true;
+      }
+      if (matrix[i][mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
     }
   }
   
-  return false
+  return false;
 };
