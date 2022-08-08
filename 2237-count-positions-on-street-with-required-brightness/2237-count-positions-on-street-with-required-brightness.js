b2f6598f3c6fef0cedcb2fd36090020eaf0f8e86
Toan Tran <toantdfu@gmail.com>
2022-08-08 07:42:26 +0700

    Time: 294 ms (40.00%), Space: 70.7 MB (40.00%) - LeetHub

--- 2237-count-positions-on-street-with-required-brightness/2237-count-positions-on-street-with-required-brightness.js
+++ 2237-count-positions-on-street-with-required-brightness/2237-count-positions-on-street-with-required-brightness.js
@@ -4,23 +4,21 @@
  * @param {number[]} requirement
  * @return {number}
  */
-var meetRequirement = function(n, lights, requirement) {
-  let prefix = new Array(n).fill(0)
-  
+function meetRequirement(n, lights, requirement) {
+  let prefix = new Array(n).fill(0);
+
   for (let [position, range] of lights) {
-    prefix[Math.max(0, position - range)] += 1
-    if (position + range + 1 <= n - 1)
-      prefix[position + range + 1] -= 1  
+    prefix[Math.max(0, position - range)] += 1;
+    const end = position + range + 1
+    if (end <= n - 1) prefix[end] -= 1;
   }
-  
-  console.log(prefix)
-  
-  let cnt = prefix[0] >= requirement[0] ? 1 : 0
+
+  let cnt = prefix[0] >= requirement[0] ? 1 : 0;
   for (let i = 1; i < n; i++) {
-    prefix[i] += prefix[i - 1]
-    
-    if (prefix[i] >= requirement[i]) cnt++
+    prefix[i] += prefix[i - 1];
+
+    if (prefix[i] >= requirement[i]) cnt++;
   }
-  
-  return cnt
-};
+
+  return cnt;
+}
