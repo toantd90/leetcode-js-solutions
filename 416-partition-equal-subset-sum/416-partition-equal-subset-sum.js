67081820f171387db5e4cee4fc213ed12c085ba6
Toan Tran <toantdfu@gmail.com>
2022-08-16 06:04:23 +0700

    Time: 271 ms (59.13%), Space: 103.8 MB (8.82%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function canPartition(nums) {
+  const sum = nums.reduce((acc, cur) => acc + cur, 0)
+  if (sum % 2 != 0) return false
+
+  const target = sum / 2
+  
+  let dp = new Array(nums.length + 1).fill().map(_ => new Array(target + 1))
+  
+  for (let i = 0; i < nums.length + 1; i++) {
+    dp[i][0] = true
+  }
+  
+  for (let i = 1; i < target + 1; i++) {
+    dp[0][i] = false
+  }
+  
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > target) return false
+    for (let j = 1; j < target + 1; j++) {
+      if (j < nums[i]) {
+        dp[i][j] = dp[i - 1][j]
+      } else {
+        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]]  
+      }
+    }
+  }
+  
+  return dp[nums.length - 1][target]
+};
