1d8b794e7d94aee9cac8ab039bf15e331bd824da
Toan Tran <toantdfu@gmail.com>
2022-08-19 07:38:55 +0700

    Time: 209 ms (46.35%), Space: 53.7 MB (64.97%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.js
+++ 904-fruit-into-baskets/904-fruit-into-baskets.js
@@ -1,16 +1,26 @@
-const totalFruit = array => {
-    let map = new Map(), max = -1
-    for(let start = 0, end = 0; end < array.length; end++){
-        let currElement = array[end]
-        map.set(currElement, map.get(currElement)+1 || 1 )
-        while(map.size > 2){
-            let char = array[start]
-            let charCount = map.get(char)
-            if(charCount-1 === 0)map.delete(char)
-            else map.set(char, charCount-1)
-            start++
-        }
-        if(map.size >= 1)max = Math.max(max, end + 1 - start)
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+function totalFruit(fruits) {
+  let baskets = new Map()
+  let maxFruits = 0
+  let startTreeIndex = 0
+  
+  for (let i = 0; i < fruits.length; i++) {
+    const fruitType = fruits[i]
+    baskets.set(fruitType, baskets.get(fruitType) + 1 || 1)
+    
+    while (baskets.size > 2) {
+      const fruitType = fruits[startTreeIndex]
+      baskets.set(fruitType, baskets.get(fruitType) - 1)
+      if (baskets.get(fruitType) == 0) baskets.delete(fruitType)
+      startTreeIndex++
     }
-    return max
+    
+    maxFruits = Math.max(maxFruits, i - startTreeIndex + 1)
+  }
+  
+  return maxFruits
+  
 };
