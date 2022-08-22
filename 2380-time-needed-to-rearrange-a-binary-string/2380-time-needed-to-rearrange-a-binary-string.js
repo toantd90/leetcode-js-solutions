eb494ffc2914bdd71a5de3998f7b7c33da8408df
Toan Tran <toantdfu@gmail.com>
2022-08-23 06:11:47 +0700

    Time: 562 ms (100.00%), Space: 48.3 MB (100.00%) - LeetHub

---
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function secondsToRemoveOccurrences(s) {
+  let cloneS = s
+  let seconds = 0
+  
+  while (cloneS.indexOf('01') >= 0) {
+    cloneS = cloneS.replaceAll('01', '10')
+    seconds++
+  }
+  
+  return seconds
+};
+
+
+// "001011"
+// "010101"
+// "101010"
+// "110100"
+// "111000"
