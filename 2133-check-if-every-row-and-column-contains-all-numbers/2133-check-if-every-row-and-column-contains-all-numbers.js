67f26984b312babf8b3fea9f21dccea77618fa7a
Toan Tran <toantdfu@gmail.com>
2022-09-11 06:55:58 +0900

    Time: 190 ms (43.53%), Space: 49.9 MB (93.33%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
@@ -5,22 +5,13 @@
 function checkValid(matrix) {
   const m = matrix.length
   
-  
   for (let i = 0; i < m; i++) {
-    let temp = new Array(m).fill(0)
+    let row = new Set()
+    let col = new Set()
     for (let j = 0; j < m; j++) {
-      const num = matrix[i][j]
-      if (temp[num - 1]) return false
-      temp[num - 1] = 1
-    }
-  }
-  
-  for (let i = 0; i < m; i++) {
-    let temp = new Array(m).fill(0)
-    for (let j = 0; j < m; j++) {
-      const num = matrix[j][i]
-      if (temp[num - 1]) return false
-      temp[num - 1] = 1
+      if (row.has(matrix[i][j]) || col.has(matrix[j][i])) return false
+      row.add(matrix[i][j])
+      col.add(matrix[j][i])
     }
   }
   
