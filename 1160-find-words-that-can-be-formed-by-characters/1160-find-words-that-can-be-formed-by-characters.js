21703ef93e73ee1a6a65a7b66f56dee5f24278f6
Toan Tran <toantdfu@gmail.com>
2022-09-11 07:47:02 +0900

    Time: 146 ms (85.64%), Space: 49.1 MB (97.42%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
@@ -1,41 +1,48 @@
 /**
+ * Count all the instances of each lowercase character in the given string
+ * and return an array with those counts in an array with indices of the
+ * alphabetical index of that character i.e. 0-25 = a-z
+ *
+ * @param {string} str
+ * @return {number[]}
+ */
+const strToCntArr = str => {
+  // Create the array of all the character counts
+  const arr = new Array(26).fill(0);
+  // Increment the count for each character in the string
+  for (let i = 0; i < str.length; i++) arr[str.charCodeAt(i) - 97]++;
+  // Return the array with all the counts
+  return arr;
+};
+
+/**
  * @param {string[]} words
  * @param {string} chars
  * @return {number}
  */
-function countCharacters(words, chars) {
-  let charsCnt = {}
-  
-  for (let char of chars) {
-    charsCnt[char] = (charsCnt[char] || 0) + 1
-  }
-  
-  let charWords = []
-  let sumLen = 0
-  for (let word of words) {
-    let charCnt = {}
-    let chars = []
-    for (let char of word) {
-      if (!charCnt[char]) {
-        charCnt[char] = 0
-        chars.push(char)
-      }
-      charCnt[char]++
-    }
-    
-    let isGood = true
-    
-    for (let i = 0; i < chars.length; i++) {
-      if (!charsCnt[chars[i]] || charCnt[chars[i]] > charsCnt[chars[i]]) {
-        isGood = false
+const countCharacters = (words, chars) => {
+  // The sum of length of the valid words
+  let sum = 0;
+  // Get the array with the counts of the characters we have
+  const have = strToCntArr(chars);
+  // Loop through the words
+  outer: for (const word of words) {
+    // Continue if we need more more total characters than we have
+    if (chars.length < word.length) continue;
+    // Get the array with the counts of the characters we need
+    const need = strToCntArr(word);
+    let canBeFormed = true
+    // Continue the outer loop when we need more characters than we have
+    for (let i = 0; i < 26; i++) {
+      if (have[i] < need[i]) {
+        canBeFormed = false
         break
-      }
+      } 
     }
-    
-    if (isGood) {
-      sumLen += word.length
-    }
+    // Any word that reaches this point is valid, so add the length to sum
+    if (canBeFormed)
+      sum += word.length;
   }
-  
-  return sumLen
+  // Return the sum of the lengths of all the valid words
+  return sum;
 };
