93440fd6591fd4c338b0e89927b460aa4a52286a
Toan Tran <toantdfu@gmail.com>
2022-09-15 06:22:33 +0900

    Time: 98 ms (36.05%), Space: 41.9 MB (76.32%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.js
+++ 362-design-hit-counter/362-design-hit-counter.js
@@ -1,95 +1,86 @@
-
-var HitCounter = function() {
-  this.logs = []
+var HitCounter = function () {
+  this.logs = [];
 };
 
-/** 
+/**
  * @param {number} timestamp
  * @return {void}
  */
-HitCounter.prototype.hit = function(timestamp) {
+HitCounter.prototype.hit = function (timestamp) {
   if (!this.logs.length) {
-    this.logs.push([timestamp, 1])
-    return
+    this.logs.push([timestamp, 1]);
+    return;
   }
-  
-  const lastLog = this.logs[this.logs.length - 1]
-  
+
+  const lastLog = this.logs[this.logs.length - 1];
+
   if (lastLog[0] == timestamp) {
-    this.logs[this.logs.length - 1] = [lastLog[0], lastLog[1] + 1]
+    this.logs[this.logs.length - 1] = [lastLog[0], lastLog[1] + 1];
   } else {
-    this.logs.push([timestamp, 1])
+    this.logs.push([timestamp, lastLog[1] + 1]);
   }
 };
 
-HitCounter.prototype.searchFirstHit = function(timestamp) {
-  let l = 0
-  let r = this.logs.length - 1
-  
-  if (timestamp < 0) timestamp = 0
-  
+HitCounter.prototype.searchFirstHit = function (timestamp) {
+  let l = 0;
+  let r = this.logs.length - 1;
+
+  if (timestamp < 0) timestamp = 0;
+
   while (l <= r) {
-    let mid = l + Math.floor((r - l) / 2)
-    
+    let mid = l + Math.floor((r - l) / 2);
+
     if (this.logs[mid][0] <= timestamp) {
-      l = mid + 1
+      l = mid + 1;
     } else {
       if (mid == l || this.logs[mid - 1][0] <= timestamp) {
-        return mid
+        return mid;
       } else {
-        r = mid - 1
+        r = mid - 1;
       }
-    } 
-  }
-  
-  return l
-}
-
-HitCounter.prototype.searchLastHit = function(timestamp) {
-  let l = 0
-  let r = this.logs.length - 1
-  
-  while (l < r) {
-    let mid = l + Math.floor((r - l) / 2)
-    
-    if (this.logs[mid][0] > timestamp) {
-      r = mid - 1
-    } else {
-      if (mid == r && this.logs[mid + 1][0] > timestamp)
-        return mid
-      else
-        l = mid + 1
     }
   }
-  
-  return r
-}
 
-/** 
+  return l;
+};
+
+HitCounter.prototype.searchLastHit = function (timestamp) {
+  let l = 0;
+  let r = this.logs.length - 1;
+
+  while (l < r) {
+    let mid = l + Math.floor((r - l) / 2);
+
+    if (this.logs[mid][0] > timestamp) {
+      r = mid - 1;
+    } else {
+      if (mid == r && this.logs[mid + 1][0] > timestamp) return mid;
+      else l = mid + 1;
+    }
+  }
+
+  return r;
+};
+
+/**
  * @param {number} timestamp
  * @return {number}
  */
-HitCounter.prototype.getHits = function(timestamp) {
-  const firstHitIndex = this.searchFirstHit(timestamp - 300)
-  const lastHitIndex = this.searchLastHit(timestamp)
-  
-  console.log(timestamp)
-  console.log(this.logs)
-  
-  console.log(firstHitIndex)
-  console.log(lastHitIndex)
-  
-  console.log('-------')
-  
-  let totalHits = 0
-  for (let i = firstHitIndex; i <= lastHitIndex; i++) {
-    totalHits +=  this.logs[i] ? this.logs[i][1] : 0
-  }
-  
-  return totalHits
+HitCounter.prototype.getHits = function (timestamp) {
+  const firstHitIndex = this.searchFirstHit(timestamp - 300);
+  const lastHitIndex = this.searchLastHit(timestamp);
+
+  return (
+    (this.logs[lastHitIndex]
+      ? this.logs[lastHitIndex][1]
+      : this.logs[this.logs.length - 1]
+      ? this.logs[this.logs.length - 1][1]
+      : 0) -
+    (this.logs[firstHitIndex - 1] ? this.logs[firstHitIndex - 1][1] : 0)
+  );
 };
 
-/** 
+/**
  * Your HitCounter object will be instantiated and called as such:
  * var obj = new HitCounter()
  * obj.hit(timestamp)
