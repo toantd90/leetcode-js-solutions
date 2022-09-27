809a1799a5f97b2493a134c8d8a0ac7d9a847d87
Toan Tran <toantdfu@gmail.com>
2022-09-28 06:47:47 +0900

    Time: 212 ms (54.10%), Space: 60.6 MB (55.74%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.js
+++ 838-push-dominoes/838-push-dominoes.js
@@ -1,9 +1,3 @@
-if (!Array.prototype.last) {
-  Array.prototype.last = function () {
-    return this[this.length - 1];
-  };
-}
-
 /**
  * @param {string} dominoes
  * @return {string}
@@ -11,32 +5,33 @@ if (!Array.prototype.last) {
 function pushDominoes(dominoes) {
   let finalStateDominoes = [];
   let lastRightIndex = -1;
-  let lastLeftIndex = -1
+  let lastLeftIndex = -1;
 
   for (let i = 0; i < dominoes.length; i++) {
+    finalStateDominoes[i] = dominoes[i];
     if (dominoes[i] == 'R') {
       lastRightIndex = i;
-      finalStateDominoes.push(dominoes[i]);
     } else if (dominoes[i] == '.') {
-      if (finalStateDominoes.last() == 'R') finalStateDominoes.push('R');
-      else finalStateDominoes.push('.');
+      if (finalStateDominoes[i - 1] == 'R') {
+        finalStateDominoes[i] = 'R'
+      };
     } else {
+      let startFillLeft = i;
       if (lastRightIndex == -1) {
-        finalStateDominoes = new Array(i + 1).fill('L');
+        startFillLeft = 0;
       } else if (lastLeftIndex > lastRightIndex) {
-        for (let j = lastLeftIndex + 1; j <= i; j++) {
-          finalStateDominoes[j] = 'L'
-        }
+        startFillLeft = lastLeftIndex + 1;
       } else {
         let mid = lastRightIndex + Math.floor((i - lastRightIndex) / 2);
         if ((i - lastRightIndex) % 2 == 0) {
           finalStateDominoes[mid] = '.';
         }
-        for (let j = mid + 1; j <= i; j++) {
-          finalStateDominoes[j] = 'L';
-        }
+        startFillLeft = mid + 1;
       }
-      lastLeftIndex = i
+      for (let j = startFillLeft; j < i; j++) {
+        finalStateDominoes[j] = 'L';
+      }
+      lastLeftIndex = i;
     }
   }
 
