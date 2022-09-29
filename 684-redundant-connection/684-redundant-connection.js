a4d6c5a820d009140a0c366e669ba03939667423
Toan Tran <toantdfu@gmail.com>
2022-09-29 10:00:31 +0900

    Time: 136 ms (28.19%), Space: 50.1 MB (11.62%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.js
@@ -0,0 +1,35 @@
+function dfs(node, visited, graph, parent = -1) {
+  if (visited[node]) {
+    return true
+  }
+  
+  visited[node] = true
+  
+  for (let neighbour of graph[node]) {
+    if (neighbour !== parent && dfs(neighbour, visited, graph, node)) {
+      return true
+    }
+  }
+  
+  return false
+}
+
+/**
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+function findRedundantConnection(edges) {
+  const n = edges.length
+  let graph = new Array(n + 1).fill().map(_ => new Array())
+
+  for (let [start, end] of edges) {
+    let visited = new Set()
+    
+    graph[start].push(end)
+    graph[end].push(start)
+    
+    if (dfs(start, visited, graph)) {
+      return [start, end]
+    }
+  }
+};
