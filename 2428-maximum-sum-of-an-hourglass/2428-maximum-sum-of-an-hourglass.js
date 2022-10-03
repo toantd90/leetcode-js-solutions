6629a279c00b9475d466bdca7d99bb4f55ee4d45
Toan Tran <toantdfu@gmail.com>
2022-10-04 05:02:21 +0900

    Time: 117 ms (50.63%), Space: 45.5 MB (45.00%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.js
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.js
@@ -6,17 +6,11 @@ function maxSum(grid) {
   const rows = grid.length
   const cols = grid[0].length
   
-  for (let row = 0; row < rows; row++) {
-    for (let col = 1; col < cols; col++) {
-      grid[row][col] += grid[row][col - 1]
-    }
-  }
-  
   let maxSum = 0
   
   for (let row = 0; row < rows - 2; row++) {
     for (let col = 0; col < cols - 2; col++) {
-      let curSum = grid[row][col + 2] - (grid[row][col - 1] || 0) + grid[row + 1][col + 1] - grid[row + 1][col] + grid[row + 2][col + 2] - (grid[row + 2][col - 1] || 0)
+      let curSum = grid[row][col] + grid[row][col + 1] + grid[row][col + 2] + grid[row + 1][col + 1] + grid[row + 2][col] + grid[row + 2][col + 1] + grid[row + 2][col + 2]
       
       maxSum = Math.max(maxSum, curSum)
     }
