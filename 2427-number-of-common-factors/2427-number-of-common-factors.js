456e8d7a89d4402247772270a5bd4777c8c75a38
Toan Tran <toantdfu@gmail.com>
2022-10-04 04:48:33 +0900

    Time: 78 ms (74.29%), Space: 41.9 MB (100.00%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.js
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -7,6 +7,7 @@ function commonFactors(a, b) {
   let numOfCommonFactors = 1
   
   for (let i = 2; i <= 1000; i++) {
+    if (i > a || i > b) break
     if (a % i == 0 && b % i == 0) {
       numOfCommonFactors++
     }
