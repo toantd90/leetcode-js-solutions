872308a45d19f8c06acac20e996a23128c1a0741
Toan Tran <toantdfu@gmail.com>
2022-10-06 05:27:58 +0900

    Time: 132 ms (74.42%), Space: 48.1 MB (63.57%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.js
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.js
@@ -1,51 +1,25 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} val
- * @param {number} depth
- * @return {TreeNode}
- */
 function addOneRow(root, val, depth) {
-  if (depth == 1) {
-    return new TreeNode(val, root)
+  if (depth === 1) {
+    const newNode = new TreeNode(val);
+    newNode.left = root;
+    return newNode;
   }
-  
-  let queue = [root]
-  let currentDepth = 1
-  let nodes = []
-  
-  while (queue.length && currentDepth < depth) {
-    const size = queue.length
-    
-    for (let i = 0; i < size; i++) {
-      const node = queue[i]
-      if (node) {
-        queue.push(node.left)
-        queue.push(node.right)
-      }
+
+  const dfs = (node, d) => {
+    if (!node) return;
+    if (d === depth - 1) {
+      const left = new TreeNode(val);
+      const right = new TreeNode(val);
+      left.left = node.left;
+      right.right = node.right;
+      node.left = left;
+      node.right = right;
+      return;
     }
-    
-    nodes = queue.splice(0, size)
-    
-    currentDepth++
-  }
-  
-  let j = 0
-  
-  for (let i = 0; i < nodes.length; i++) {
-    if (nodes[i]) {
-      nodes[i].left = new TreeNode(val, queue[j])
-      nodes[i].right = new TreeNode(val, null, queue[j + 1])
-      j += 2
-    }
-  }
-    
-  return root
-};
+    dfs(node.left, d + 1);
+    dfs(node.right, d + 1);
+  };
+
+  dfs(root, 1);
+  return root;
+}
