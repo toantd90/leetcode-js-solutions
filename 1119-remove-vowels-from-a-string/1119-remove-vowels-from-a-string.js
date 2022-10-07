50f909ac75174e4df772d3a38e215274b4e5c4ad
Toan Tran <toantdfu@gmail.com>
2022-10-08 06:13:21 +0900

    Time: 82 ms (64.67%), Space: 42.1 MB (53.80%) - LeetHub

--- 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.js
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.js
@@ -3,12 +3,5 @@
  * @return {string}
  */
 function removeVowels(s) {
-  let nonVowelsStr = ''
-  for (let i = 0; i < s.length; i++) {
-    if (!'aeiou'.includes(s[i])) {
-      nonVowelsStr += s[i]
-    }
-  }
-  
-  return nonVowelsStr
+  return s.replace(/[aeiou]/gi, '');
 };
