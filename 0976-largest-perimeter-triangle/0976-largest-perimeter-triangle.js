edb580abe2fe29ed31ba1c270398b164064cfb7e
Toan Tran <toantdfu@gmail.com>
2022-10-12 12:38:10 +0900

    Time: 192 ms (12.79%), Space: 45.9 MB (22.09%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
@@ -3,12 +3,15 @@
  * @return {number}
  */
 function largestPerimeter(nums) {
-  nums.sort((a, b) => a - b);
-  for (let i = nums.length - 1; i > 1; i--) {
-    if (nums[i] < nums[i - 1] + nums[i - 2]) {
+  nums = nums.sort((a, b) => a - b);
+  for (let i = nums.length - 1; i >= 2; i--) {
+    if (
+      nums[i] + nums[i - 1] > nums[i - 2] &&
+      nums[i - 2] + nums[i - 1] > nums[i] &&
+      nums[i - 2] + nums[i - 1] > nums[i - 1]
+    ) {
       return nums[i] + nums[i - 1] + nums[i - 2];
     }
   }
-
   return 0;
 }
