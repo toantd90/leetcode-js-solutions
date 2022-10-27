7268c4a647d6492c262692b56ab1b937ba21bc66
Toan Tran <toantdfu@gmail.com>
2022-10-28 05:40:55 +0900

    Time: 132 ms (36.84%), Space: 44.1 MB (37.29%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,16 +4,19 @@
  * @return {boolean}
  */
 function isIsomorphic(s, t) {
-  let mapS = {}
-  let mapT = {}
+  let mapS = {};
+  let mapT = {};
   for (let i = 0; i < s.length; i++) {
-    if ((mapS[s[i]] && mapS[s[i]] != t[i]) || (mapT[t[i]] && mapT[t[i]] !== s[i])) {
-      return false
+    if (
+      (mapS[s[i]] && mapS[s[i]] != t[i]) ||
+      (mapT[t[i]] && mapT[t[i]] !== s[i])
+    ) {
+      return false;
     }
-    
-    mapS[s[i]] = t[i]
-    mapT[t[i]] = s[i]
+
+    mapS[s[i]] = t[i];
+    mapT[t[i]] = s[i];
   }
-  
-  return true
-};
+
+  return true;
+}
