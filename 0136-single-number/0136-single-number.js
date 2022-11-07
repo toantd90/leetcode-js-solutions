95e212977697c0e519bcf5190250c627cad79d37
Toan Tran <toantdfu@gmail.com>
2022-11-08 06:40:39 +0900

    Time: 111 ms (63.74%), Space: 44.7 MB (64.90%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  let res = 0;
+
+  for (let num of nums) {
+    res ^= num;
+  }
+
+  return res;
+};
