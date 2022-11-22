8c08d934d97de80990c9bb97d1db84f3e3fc99fc
Toan Tran <toantdfu@gmail.com>
2022-11-23 07:05:18 +0900

    Time: 142 ms (85.53%), Space: 52.6 MB (18.33%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.js
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.js
@@ -1,39 +1,39 @@
 class UnionFind {
   constructor(numOfSet) {
-    this.parents = []
-    this.ranks = []
-    this.numOfSet = numOfSet
-    
+    this.parents = [];
+    this.ranks = [];
+    this.numOfSet = numOfSet;
+
     for (let i = 0; i < numOfSet; i++) {
-      this.parents[i] = i
-      this.ranks[i] = 1
+      this.parents[i] = i;
+      this.ranks[i] = 1;
     }
   }
-  
+
   findSet(index) {
     if (this.parents[index] !== index) {
-      this.parents[index] = this.findSet(this.parents[index])
+      this.parents[index] = this.findSet(this.parents[index]);
     }
-    
-    return this.parents[index]
+
+    return this.parents[index];
   }
-  
+
   unionSet(index1, index2) {
-    const parent1 = this.findSet(index1)
-    const parent2 = this.findSet(index2)
-    
+    const parent1 = this.findSet(index1);
+    const parent2 = this.findSet(index2);
+
     if (parent1 === parent2) {
-      return
+      return;
     }
-    
-    this.numOfSet--
+
+    this.numOfSet--;
     if (this.ranks[parent1] > this.ranks[parent2]) {
-      this.parents[parent2] = parent1
+      this.parents[parent2] = parent1;
     } else if (this.ranks[parent1] < this.ranks[parent2]) {
-      this.parents[parent1] = parent2
+      this.parents[parent1] = parent2;
     } else {
-      this.parents[parent1] = parent2
-      this.ranks[parent2]++
+      this.parents[parent1] = parent2;
+      this.ranks[parent2]++;
     }
   }
 }
@@ -42,41 +42,41 @@ class UnionFind {
  * @param {number[][]} stones
  * @return {number}
  */
-var removeStones = function(stones) {
-  let cols = {}
-  let rows = {}
-  
-  let unionFind = new UnionFind(stones.length)
-  
+function removeStones(stones) {
+  let cols = {};
+  let rows = {};
+
+  let unionFind = new UnionFind(stones.length);
+
   for (let i = 0; i < stones.length; i++) {
-    const [row, col] = stones[i]
-    
+    const [row, col] = stones[i];
+
     if (!rows[row]) {
-      rows[row] = []
+      rows[row] = [];
     }
-    rows[row].push(i)
-    
+    rows[row].push(i);
+
     if (!cols[col]) {
-      cols[col] = []
+      cols[col] = [];
     }
-    cols[col].push(i)
+    cols[col].push(i);
   }
-  
+
   for (let [_, indexes] of Object.entries(rows)) {
-    const firstIndex = indexes[0]
-    
+    const firstIndex = indexes[0];
+
     for (let i = 1; i < indexes.length; i++) {
-      unionFind.unionSet(firstIndex, indexes[i])
+      unionFind.unionSet(firstIndex, indexes[i]);
     }
   }
-  
+
   for (let [_, indexes] of Object.entries(cols)) {
-    const firstIndex = indexes[0]
-    
+    const firstIndex = indexes[0];
+
     for (let i = 1; i < indexes.length; i++) {
-      unionFind.unionSet(firstIndex, indexes[i])
+      unionFind.unionSet(firstIndex, indexes[i]);
     }
   }
-  
-  return stones.length - unionFind.numOfSet
+
+  return stones.length - unionFind.numOfSet;
 };
