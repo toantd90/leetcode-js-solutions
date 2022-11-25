be5834a2c218a45513efe0e8fa6e439ee29a74b8
Toan Tran <toantdfu@gmail.com>
2022-11-26 07:48:44 +0900

    Time: 70 ms (98.71%), Space: 44.2 MB (97.99%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -2,21 +2,16 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findErrorNums = function(nums) {
-  
+function findErrorNums(nums) {
   for (let i = 0; i < nums.length; i++) {
     while (nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
-      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
     }
   }
-  
+
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== i + 1) {
-      return [nums[i], i + 1]
+      return [nums[i], i + 1];
     }
   }
-};
-
-
-
-
+}
