b3de0598443333c554a889e02e6d65a883bed66c
Toan Tran <toantdfu@gmail.com>
2022-11-28 05:15:15 +0900

    Time: 232 ms (57.14%), Space: 60.7 MB (78.57%) - LeetHub

---
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} heights
+ * @return {number[]}
+ */
+function findBuildings(heights) {
+  let stack = [0];
+
+  for (let i = 0; i < heights.length; i++) {
+    while (stack.length > 0 && heights[stack[stack.length - 1]] <= heights[i]) {
+      stack.pop();
+    }
+
+    stack.push(i);
+  }
+
+  return stack;
+}
