f8f05e997a8f85a410fcf6e654f6c2c3bf57c869
Toan Tran <toantdfu@gmail.com>
2023-01-10 06:52:26 +0900

    Time: 814 ms (27.17%), Space: 76.1 MB (29.35%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function maxKelements(nums, k) {
+  let maxHeap = new MaxPriorityQueue({ priority: (num) => num });
+
+  let score = 0;
+
+  for (let num of nums) {
+    maxHeap.enqueue(num);
+  }
+
+  for (let i = 0; i < k; i++) {
+    const currentNum = maxHeap.dequeue().element;
+
+    score += currentNum;
+
+    maxHeap.enqueue(Math.ceil(currentNum / 3));
+  }
+
+  return score;
+}
