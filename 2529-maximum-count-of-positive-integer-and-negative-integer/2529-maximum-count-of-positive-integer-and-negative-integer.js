f1af9031de0c4101552cb7b0a55ed25d51465975
Toan Tran <toantdfu@gmail.com>
2023-01-18 11:49:49 +0900

    Time: 72 ms (43.61%), Space: 44.3 MB (41.95%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -1,36 +1,24 @@
+function lowerBound(arr, start, end, value) {
+  while (start < end) {
+    const mid = start + Math.floor((end - start) / 2)
+    
+    if (arr[mid] < value) {
+      start = mid + 1
+    } else {
+      end = mid
+    }
+  }
+  
+  return start
+}
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 function maximumCount(nums) {
-  let l = 0;
-  let r = nums.length;
+  const firstPositiveIndex = lowerBound(nums, 0, nums.length, 1)  
+  const firstZeroIndex = lowerBound(nums, 0, firstPositiveIndex, 0)
 
-  while (l < r) {
-    const mid = l + Math.floor((r - l) / 2);
-    if (nums[mid] <= 0) {
-      l = mid + 1;
-    } else {
-      r = mid;
-    }
-  }
-
-  const firstPositiveIndex = r;
-
-  l = -1;
-  while (l <= r) {
-    const mid = l + Math.floor((r - l) / 2);
-    if (nums[mid] >= 0) {
-      r = mid - 1;
-    } else {
-      l = mid + 1;
-    }
-  }
-
-  const lastNegativeIndex = r;
-
-  return Math.min(
-    nums.length,
-    Math.max(lastNegativeIndex + 1, nums.length - firstPositiveIndex)
-  );
+  return Math.max(firstZeroIndex, nums.length - firstPositiveIndex)
 }
