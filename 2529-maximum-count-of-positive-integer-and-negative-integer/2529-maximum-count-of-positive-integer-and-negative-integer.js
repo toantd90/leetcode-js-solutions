8fc5558e2f0c184be97019e3e8632415b6eec0f2
Toan Tran <toantdfu@gmail.com>
2023-01-18 10:12:02 +0900

    Time: 67 ms (71.28%), Space: 44 MB (60.60%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -3,16 +3,34 @@
  * @return {number}
  */
 function maximumCount(nums) {
-  let i = 0;
+  let l = 0;
+  let r = nums.length;
 
-  while (nums[i] < 0) {
-    i++;
+  while (l < r) {
+    const mid = l + Math.floor((r - l) / 2);
+    if (nums[mid] <= 0) {
+      l = mid + 1;
+    } else {
+      r = mid;
+    }
   }
 
-  let j = i;
-  while (nums[j] == 0) {
-    j++;
+  const firstPositiveIndex = r;
+
+  l = -1;
+  while (l <= r) {
+    const mid = l + Math.floor((r - l) / 2);
+    if (nums[mid] >= 0) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
   }
 
-  return Math.max(i, nums.length - j);
+  const lastNegativeIndex = r;
+
+  return Math.min(
+    nums.length,
+    Math.max(lastNegativeIndex + 1, nums.length - firstPositiveIndex)
+  );
 }
