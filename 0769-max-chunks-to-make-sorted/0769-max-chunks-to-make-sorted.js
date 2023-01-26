d34dabb00594dfd73b25966b6dd4f77800c59ed6
Toan Tran <toantdfu@gmail.com>
2023-01-27 04:02:11 +0700

    Time: 69 ms (49.30%), Space: 41.7 MB (73.24%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+function maxChunksToSorted(arr) {
+  let max = 0
+  let count = 0
+  
+  for (let i = 0; i < arr.length; i++) {
+    max = Math.max(max, arr[i])
+    
+    if (max === i) {
+      count++
+    }
+  }
+  
+  return count
+};
