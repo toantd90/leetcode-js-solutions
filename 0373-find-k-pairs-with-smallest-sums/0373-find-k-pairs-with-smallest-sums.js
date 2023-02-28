d31242d758e9899b6939fe51e72f973e03b66f73
Toan Tran <toantdfu@gmail.com>
2023-03-01 02:53:05 +0700

    Time: 437 ms (41.18%), Space: 82.5 MB (15.29%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.js
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.js
@@ -5,25 +5,27 @@
  * @return {number[][]}
  */
 function kSmallestPairs(nums1, nums2, k) {
-  let maxHeap = new MaxPriorityQueue({
-    priority: ([num1, num2]) => num1 + num2,
+  let i1 = 0;
+  let i2 = 0;
+
+  let minHeap = new MinPriorityQueue({
+    priority: ([num1, num2, _]) => num1 + num2,
   });
 
-  for (let i = 0; i < Math.min(nums1.length, k); i++) {
-    for (let j = 0; j < Math.min(nums2.length, k); j++) {
-      if (maxHeap.size() < k) {
-        maxHeap.enqueue([nums1[i], nums2[j]]);
-      } else {
-        if (
-          maxHeap.front().element[0] + maxHeap.front().element[1] >
-          nums1[i] + nums2[j]
-        ) {
-          maxHeap.dequeue();
-          maxHeap.enqueue([nums1[i], nums2[j]]);
-        }
-      }
-    }
+  for (let num of nums1) {
+    minHeap.enqueue([num, nums2[0], 0]);
   }
 
-  return maxHeap.toArray().map(({ element }) => element);
+  let ans = [];
+
+  while (k-- > 0 && minHeap.size() > 0) {
+    const [num1, num2, index] = minHeap.dequeue().element;
+    ans.push([num1, num2]);
+
+    if (index === nums2.length - 1) continue;
+
+    minHeap.enqueue([num1, nums2[index + 1], index + 1]);
+  }
+
+  return ans;
 }
