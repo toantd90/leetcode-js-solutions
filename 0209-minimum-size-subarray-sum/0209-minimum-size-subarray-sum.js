9861384a531c59f645ae8b99e0301d22a2f86fde
Toan Tran <toantdfu@gmail.com>
2023-03-03 03:39:23 +0700

    Time: 65 ms (88.94%), Space: 46.8 MB (10.73%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minSubArrayLen(target, nums) {
+  let left = 0;
+  let right = 0;
+  let sum = 0;
+  let minLength = nums.length + 1;
+
+  while (right < nums.length) {
+    while (sum < target) {
+      sum += nums[right++];
+    }
+
+    minLength = Math.min(minLength, right - left);
+
+    while (sum >= target) {
+      sum -= nums[left++];
+    }
+    
+    minLength = Math.min(minLength, right - left + 1);
+  }
+
+  return minLength === nums.length + 1 ? 0 : minLength;
+}
