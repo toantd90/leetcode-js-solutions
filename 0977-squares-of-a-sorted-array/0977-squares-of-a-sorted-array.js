028d07dbece0d0a2565b37dc266273dd0f05c669
Toan Tran <toantdfu@gmail.com>
2023-03-04 18:24:44 +0900

    Time: 102 ms (81.96%), Space: 47.6 MB (98.06%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,19 +2,19 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-  let left = 0
-  let right = nums.length - 1
-  
-  let answer = []
-  
-  while (left <= right) {
+const sortedSquares = (nums) => {
+  const n = nums.length;
+  let left = 0;
+  let right = n - 1;
+  let result = new Array(n);
+
+  for (let i = n - 1; i >= 0; i--) {
     if (Math.abs(nums[left]) > Math.abs(nums[right])) {
-      answer.push(Math.pow(nums[left++], 2))
+      result[i] = Math.pow(nums[left++], 2);
     } else {
-      answer.push(Math.pow(nums[right--], 2))
+      result[i] = Math.pow(nums[right--], 2);
     }
   }
-  
-  return answer.reverse()
+
+  return result;
 };
