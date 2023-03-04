96100684934fea5a80e38032dece96b7211eee9b
Toan Tran <toantdfu@gmail.com>
2023-03-05 06:19:33 +0900

    Time: 62 ms (82.95%), Space: 42.4 MB (42.95%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -3,10 +3,43 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) return true;
-  }
-
-  return false;
+const search = (nums, target) => {
+    let l = 0
+    let r = nums.length - 1
+    
+    while (l <= r) {
+        
+        
+        while (l < r && nums[l] == nums[l + 1]) {
+          l++
+        }
+      
+      while (l < r && nums[r] == nums[r - 1]) {
+          r--
+        }
+      
+      const mid = Math.floor((l + r) / 2)
+        
+        if (nums[mid] === target) {
+            return true
+        } else if (nums[mid] > nums[r]) {
+            // pivot in range mid .. r
+            // l to mid is normal
+           if (nums[l] <= target && target <= nums[mid]) {
+               r = mid - 1
+           } else {
+               l = mid + 1
+           }
+        } else {
+            // pivot in range l   .. mid
+            // mid + 1 to r is normal
+          if (nums[mid + 1] <= target && target <= nums[r]) {
+              l = mid + 1
+          } else {
+              r = mid - 1
+          }
+        }
+    }
+    
+    return false
 };
