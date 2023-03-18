f7a7d91b89a20e18e0266cf3552c7f6dcdf6800f
Toan Tran <toantdfu@gmail.com>
2023-03-19 05:13:58 +0700

    Time: 841 ms (50.93%), Space: 48.7 MB (51.63%) - LeetHub

--- 0494-target-sum/0494-target-sum.js
+++ 0494-target-sum/0494-target-sum.js
@@ -1,35 +1,39 @@
+function calculate(nums, sum, remain, index, memo) {
+  if (index === nums.length) {
+    if (remain === 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  if (memo[index][remain + sum]) {
+    return memo[index][remain + sum];
+  }
+
+  const addition = calculate(nums, sum, remain + nums[index], index + 1, memo);
+  const subtraction = calculate(
+    nums,
+    sum,
+    remain - nums[index],
+    index + 1,
+    memo
+  );
+
+  memo[index][remain + sum] = addition + subtraction;
+
+  return memo[index][remain + sum];
+}
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
-let goal = 0;
-let sum = 0;
-var findTargetSumWays = function (nums, target) {
-  const len = nums.length;
-  sum = nums.reduce((a, b) => a + b);
-  goal = target;
-  // why sum * 2 + 1? all possible subsum ranges from all negative to all positive plus zero in the middle
-  const memo = new Array(len)
-    .fill(null)
-    .map(() => new Array(sum * 2 + 1).fill(null));
-  const res = dfs(nums, 0, memo, 0);
-  return res;
-};
+function findTargetSumWays(nums, target) {
+  const sum = nums.reduce((a, b) => a + b);
 
-const dfs = (nums, total, memo, idx) => {
-  if (idx === nums.length) {
-    if (total === goal) return 1;
-    else return 0;
-  }
+  let memo = new Array(nums.length).fill().map((_) => new Array(2 * sum + 1).fill(null));
 
-  // we make the negative total's index valid by adding the sum to it
-  if (memo[idx][total + sum] !== null) return memo[idx][total + sum];
-
-  const pos = dfs(nums, total + nums[idx], memo, idx + 1);
-  const neg = dfs(nums, total - nums[idx], memo, idx + 1);
-
-  memo[idx][total + sum] = pos + neg;
-
-  return memo[idx][total + sum];
-};
+  return calculate(nums, sum, target, 0, memo);
+}
