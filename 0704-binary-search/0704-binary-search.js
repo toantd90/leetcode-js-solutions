90b39441eb3de3cde915bba69c0822144a08e792
Toan Tran <toantdfu@gmail.com>
2023-04-02 06:33:00 +0900

    Time: 67 ms (53.56%), Space: 45.2 MB (27.98%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = (nums, target) => {
+    let l = 0, r = nums.length - 1
+    
+    while (l <= r) {
+        const mid = l + Math.floor((r - l) / 2)
+        
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
+            r = mid - 1
+        } else {
+            l = mid + 1
+        }
+    }
+    
+    return -1
+};
