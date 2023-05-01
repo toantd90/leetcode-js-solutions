0ef198da518c0fb23265be62c8dd4a784191cae5
Toan Tran <toantdfu@gmail.com>
2023-05-02 05:43:19 +0900

    Time: 68 ms (33.21%), Space: 43.5 MB (22.74%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+  let longestPrefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    let prefix = "";
+    for (let j = 0; j < Math.min(longestPrefix.length, strs[i].length); j++) {
+      if (longestPrefix[j] !== strs[i][j]) {
+        break;
+      }
+      prefix += strs[i][j];
+    }
+
+    if (!prefix) {
+      return "";
+    }
+
+    longestPrefix = prefix;
+  }
+
+  return longestPrefix;
+}
+
+longestCommonPrefix(["ab", "a"]);
