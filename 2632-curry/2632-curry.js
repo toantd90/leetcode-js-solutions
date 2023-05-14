50f6352db6861dfa2540069c105aaae7f64b5832
Toan Tran <toantdfu@gmail.com>
2023-05-15 08:38:59 +0900

    Time: 63 ms (81.69%), Space: 49.1 MB (12.05%) - LeetHub

---
+++ 2632-curry/2632-curry.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+
+    return (...nextArgs) => curried(...args, ...nextArgs);
+  };
+}
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
