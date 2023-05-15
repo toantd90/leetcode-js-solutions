854001e3b473d1e43d0ea6404dd3f020839736fd
Toan Tran <toantdfu@gmail.com>
2023-05-16 06:11:22 +0900

    Time: 61 ms (48.15%), Space: 41.8 MB (59.54%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -2,10 +2,8 @@
  * @param {number} millis
  */
 async function sleep(millis) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(millis);
-    }, millis);
+  return new Promise((res) => {
+    setTimeout(res, millis);
   });
 }
 
