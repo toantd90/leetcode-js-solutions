c6c2ce1d91bbfd88d0b54adbf30add633b39d156
Toan Tran <toantdfu@gmail.com>
2023-06-14 09:00:21 +0900

    Time: 62 ms (40.00%), Space: 42.5 MB (9.76%) - LeetHub

--- 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.js
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.js
@@ -3,11 +3,13 @@
  * @return {string}
  */
 function maximumTime(time) {
-  let latestTime = []
-  latestTime.push(time[0] !== "?" ? time[0] : time[1] == "?" || time[1] <= "3" ? "2" : "1")
-  latestTime.push(time[1] !== "?" ? time[1] : latestTime[0] == "2" ? "3" : "9")
-  latestTime.push(':')
-    latestTime.push(time[3] = time[3] != "?" ? time[3] : "5")
-    latestTime.push(time[4] = time[4] != "?" ? time[4] : "9");
-  return latestTime.join('');
+  let latestTime = [];
+  latestTime.push(
+    time[0] !== "?" ? time[0] : time[1] == "?" || time[1] <= "3" ? "2" : "1"
+  );
+  latestTime.push(time[1] !== "?" ? time[1] : latestTime[0] == "2" ? "3" : "9");
+  latestTime.push(":");
+  latestTime.push((time[3] = time[3] != "?" ? time[3] : "5"));
+  latestTime.push((time[4] = time[4] != "?" ? time[4] : "9"));
+  return latestTime.join("");
 }
