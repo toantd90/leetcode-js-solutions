68d5abc619f1ec9b3523ff38fbfa180853731fee
Toan Tran <toantdfu@gmail.com>
2023-07-01 19:16:47 +0900

    Time: 63 ms (22.19%), Space: 41.5 MB (91.77%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.js
+++ 0500-keyboard-row/0500-keyboard-row.js
@@ -3,35 +3,15 @@
  * @return {string[]}
  */
 function findWords(words) {
-  const firstRow = "qwertyuiop";
-  const secondRow = "asdfghjkl";
-  const thirdRow = "zxcvbnm";
-  let oneRowWords = [];
+  const rows = [
+    new Set(["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]),
+    new Set(["a", "s", "d", "f", "g", "h", "j", "k", "l"]),
+    new Set(["z", "x", "c", "v", "b", "n", "m"]),
+  ];
 
-  for (let word of words) {
-    let wordByRows = [0, 0, 0];
-    let isOneRow = true;
-    const lowerCaseWord = word.toLowerCase();
-
-    for (let i = 0; i < lowerCaseWord.length; i++) {
-      if (firstRow.includes(lowerCaseWord[i])) {
-        wordByRows[0]++;
-      } else if (secondRow.includes(lowerCaseWord[i])) {
-        wordByRows[1]++;
-      } else {
-        wordByRows[2]++;
-      }
-
-      if (wordByRows.every((count) => count !== i + 1)) {
-        isOneRow = false;
-        break;
-      }
-    }
-
-    if (isOneRow) {
-      oneRowWords.push(word);
-    }
-  }
-
-  return oneRowWords;
+  return words.filter((word) => {
+    const lowerWord = word.toLowerCase();
+    const row = rows.find((set) => set.has(lowerWord[0]));
+    return word.split("").every((letter) => row.has(letter.toLowerCase()));
+  });
 }
