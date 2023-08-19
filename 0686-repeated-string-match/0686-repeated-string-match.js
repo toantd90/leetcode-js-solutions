c657f6f34efa458e4696f720a6b8bcd241822761
Toan Tran <toantdfu@gmail.com>
2023-08-19 07:01:56 +0700

    Time: 52 ms (89.66%), Space: 41.7 MB (98.28%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.js
+++ 0686-repeated-string-match/0686-repeated-string-match.js
@@ -4,10 +4,10 @@
  * @return {number}
  */
 function repeatedStringMatch(a, b) {
-  const maxStr = a.repeat(Math.ceil((a.length + b.length) / a.length))
-  const indexOfB = maxStr.indexOf(b)
-  
-  if (indexOfB === -1) return -1
-  
-  return Math.ceil((indexOfB + b.length) / a.length)
-};
+  const maxStr = a.repeat(Math.ceil((a.length + b.length) / a.length));
+  const indexOfB = maxStr.indexOf(b);
+
+  if (indexOfB === -1) return -1;
+
+  return Math.ceil((indexOfB + b.length) / a.length);
+}
