c44866ac7e3be239d25aac99a6d43d81e66a711b
Toan Tran <toantdfu@gmail.com>
2023-08-20 15:29:39 +0700

    Time: 160 ms (100.00%), Space: 50.8 MB (100.00%) - LeetHub

---
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+function minimumSum(n, k) {
+  let sum = 0;
+  let nums = new Set();
+  let num = 1;
+  for (let i = 0; num <= k && i < n; i++) {
+    while (nums.has(k - num)) {
+      num++;
+    }
+    sum += num;
+    nums.add(num);
+    num++;
+  }
+  
+  console.log(nums)
+  
+  if (nums.size < n) {
+    sum += ((k + (k + n - nums.size + 1)) * (n - nums.size)) / 2;
+  }
+
+  return sum;
+}
