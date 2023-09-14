11714fbfb54df80f94ca51ddbfda3cbb6a16de6d
Toan Tran <toantdfu@gmail.com>
2023-09-15 08:58:48 +0900

    Time: 62 ms (52.40%), Space: 43.7 MB (91.70%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+function minMovesToSeat(seats, students) {
+  seats.sort((s1, s2) => s1 - s2)   
+  students.sort((s1, s2) => s1 - s2)
+  
+  let moves = 0
+  for (let i = 0; i < seats.length; i++) {
+    moves += Math.abs(seats[i] - students[i])
+  }
+  
+  return moves
+};
