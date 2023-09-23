febdf34fda1f94fcdcf8bcff0695f4a734be6708
Toan Tran <toantdfu@gmail.com>
2023-09-24 08:40:10 +0900

    Time: 80 ms (86.03%), Space: 49.7 MB (53.63%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.js
+++ 2363-merge-similar-items/2363-merge-similar-items.js
@@ -3,21 +3,25 @@
  * @param {number[][]} items2
  * @return {number[][]}
  */
-function mergeSimilarItems(items1, items2) {
-  let valueWeightMap = new Map();
-  for (let i = 0; i < items1.length; i++) {
-    valueWeightMap.set(items1[i][0], items1[i][1]);
-  }
-
-  for (let i = 0; i < items2.length; i++) {
-    valueWeightMap.set(
-      items2[i][0],
-      (valueWeightMap.get(items2[i][0]) || 0) + items2[i][1]
-    );
-  }
-
-  const values = Array.from(valueWeightMap.keys());
-  const sortedValues = values.sort((v1, v2) => v1 - v2);
-
-  return sortedValues.map((value) => [value, valueWeightMap.get(value)]);
-}
+var mergeSimilarItems = function(items1, items2) {
+    let outPut = {}
+    items1.forEach((item) => {
+        if(outPut[item[0]] == null){
+            outPut[item[0]] = item[1]
+        } else {
+            outPut[item[0]] = outPut[item[0]] + item[1]
+        }
+    })
+    items2.forEach((item) => {
+        if(outPut[item[0]] == null){
+            outPut[item[0]] = item[1]
+        } else {
+            outPut[item[0]] = outPut[item[0]] + item[1]
+        }
+    })
+    let output2 = []
+    Object.keys(outPut).forEach((key) => {
+        output2.push([key, outPut[key]])
+    })
+    return output2
+};
