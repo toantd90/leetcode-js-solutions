911a057725c81a7a5fd5a7edb24d563821296d51
Toan Tran <toantdfu@gmail.com>
2023-09-26 09:42:30 +0900

    Time: 63 ms (8.67%), Space: 45.2 MB (5.26%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums) {
+  let l = 0
+  let r = nums.length - 1
+  
+  while (l < r) {
+    while (nums[l] === 0) {
+      l++
+    }
+    
+    while (l < r && nums[r] === 2) {
+      r--
+    }
+    
+    if (l < r && (nums[r] === 0 || nums[l] === 2)) {
+      [nums[l], nums[r]] = [nums[r], nums[l]]
+    }
+    
+    r--
+    
+    // nums[l] === 1 || nums[l] === 2
+    // nums[r] === 0 || nums[r] === 1
+  }
+  
+  console.log(nums)
+  
+  
+  r = nums.length - 1
+  while (l < r) {
+    while (nums[r] === 2) {
+      r--
+    }
+    
+    if (l < r && nums[l] === 2) {
+      [nums[l], nums[r]] = [nums[r], nums[l]]
+    }
+    
+    l++
+  }
+};
