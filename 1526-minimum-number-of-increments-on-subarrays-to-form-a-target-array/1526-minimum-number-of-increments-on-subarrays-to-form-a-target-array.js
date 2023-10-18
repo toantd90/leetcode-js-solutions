7364a1443d63bde0373b140cca6ce42173d83ad9
Toan Tran <toantdfu@gmail.com>
2023-10-18 13:27:23 +0900

    Time: 61 ms (100.00%), Space: 51 MB (38.89%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.js
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 function minNumberOperations(target) {
-  let numOfOperations = target[0]
-  
+  let numOfOperations = target[0];
+
   for (let i = 1; i < target.length; i++) {
-    numOfOperations += Math.max(target[i] - target[i - 1], 0)
+    numOfOperations += Math.max(target[i] - target[i - 1], 0);
   }
-  
-  return numOfOperations
-};
+
+  return numOfOperations;
+}
