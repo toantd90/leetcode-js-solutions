29ba54a7fd4bd6da08ef3f71ef3522edaaf1d42d
Toan Tran <toantdfu@gmail.com>
2023-11-19 13:05:01 +0900

    Time: 47 ms (85.06%), Space: 42 MB (29.33%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -7,15 +7,9 @@ function fib(n) {
   dp[0] = 0
   dp[1] = 1
   
-  if (n < 2) {
-    return dp[n]
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
   }
-  
-  if (dp[n]) {
-    return dp[n]
-  }
-  
-  dp[n] = fib(n - 1) + fib(n - 2)
   
   return dp[n]
 };
