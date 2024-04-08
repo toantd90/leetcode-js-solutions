ff6ff7938cc21086852662c06cfba7a9e582bdaa
Toan Tran <toantdfu@gmail.com>
2024-04-08 12:33:09 +0900

    Time: 181 ms (85.71%), Space: 68.4 MB (85.71%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.js
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.js
@@ -4,32 +4,20 @@
  * @return {number}
  */
 function findSmallestInteger(nums, value) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > 0 && nums[i] >= value) {
-      nums[i] -= Math.floor(nums[i] / value) * value;
-    } else if (nums[i] < 0 && nums[i] < value) {
-      nums[i] += Math.ceil(-nums[i] / value) * value;
-    } else {
-      continue;
-    }
-  }
-
   let count = {};
-  for (let i = 0; i < nums.length; i++) {
-    count[nums[i]] = (count[nums[i]] || 0) + 1;
-
-    if (count[nums[i]] > 1) {
-      nums[i] += value * (count[nums[i]] - 1);
-    }
+  for (let num of nums) {
+    const remainder = (value + num % value) % value;
+    count[remainder] = (count[remainder] || 0) + 1;
   }
-
-  nums.sort((n1, n2) => n1 - n2);
-
+  
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== i) {
+    if (!count[i % value] || count[i % value] < 0) {
       return i;
     }
+    count[i % value]--;
   }
+
+  
 
   return nums.length;
 }
