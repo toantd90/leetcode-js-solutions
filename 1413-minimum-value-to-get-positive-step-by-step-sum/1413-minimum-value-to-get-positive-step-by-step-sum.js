d8ccf3dcb57b56456d0cb7a283d237eb02d0c78c
Toan Tran <toantdfu@gmail.com>
2024-04-11 05:55:54 +0900

    Time: 88 ms (5.16%), Space: 54.2 MB (5.16%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -1,15 +1,35 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function minStartValue(nums) {
+//   let prefixSum = nums[0];
+//   let minPrefixSum = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     prefixSum += nums[i];
+//     minPrefixSum = Math.min(minPrefixSum, prefixSum);
+//   }
+
+//   return minPrefixSum > 0 ? 1 : 1 - minPrefixSum;
+// }
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
-function minStartValue(nums) {
-  let prefixSum = nums[0];
-  let minPrefixSum = nums[0];
-
-  for (let i = 1; i < nums.length; i++) {
-    prefixSum += nums[i];
-    minPrefixSum = Math.min(minPrefixSum, prefixSum);
-  }
-
-  return minPrefixSum > 0 ? 1 : 1 - minPrefixSum;
-}
+var minStartValue = function(nums) {
+    let sum = nums[0];
+    let min = nums[0];
+    
+    let j = 1;
+    
+    while (j < nums.length) {
+        sum += nums[j];
+        min = Math.min(min, sum);
+        console.log(sum, min)
+        j++;
+    }
+    
+    return min > 0 ? 1 : Math.abs(min) + 1;
+};
