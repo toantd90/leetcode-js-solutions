da594a1b4822fcb1aa7ee9c6f1d4f7e8592725b3
Toan Tran <toantdfu@gmail.com>
2024-04-18 08:58:05 +0900

    Time: 56 ms (38.79%), Space: 49.5 MB (14.27%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -1,40 +1,27 @@
+function sortTwoColors(nums, color, startIndex) {
+  let l = startIndex;
+  let r = nums.length;
+
+  while (l <= r) {
+    if (nums[l] === color) {
+      l++;
+    } else if (nums[r] !== color) {
+      r--;
+    } else {
+      [nums[l], nums[r]] = [nums[r], nums[l]];
+      l++;
+      r--;
+    }
+  }
+
+  return l;
+}
+
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 function sortColors(nums) {
-  let l = 0;
-  let r = nums.length - 1;
-
-  while (l < r) {
-    while (nums[l] === 0) {
-      l++;
-    }
-
-    while (l < r && nums[r] === 2) {
-      r--;
-    }
-
-    if (l < r && (nums[r] === 0 || nums[l] === 2)) {
-      [nums[l], nums[r]] = [nums[r], nums[l]];
-    }
-
-    r--;
-
-    // nums[l] === 1 || nums[l] === 2
-    // nums[r] === 0 || nums[r] === 1
-  }
-
-  r = nums.length - 1;
-  while (l < r) {
-    while (nums[r] === 2) {
-      r--;
-    }
-
-    if (l < r && nums[l] === 2) {
-      [nums[l], nums[r]] = [nums[r], nums[l]];
-    }
-
-    l++;
-  }
+  const lastZeroIndex = sortTwoColors(nums, 0, 0);
+  sortTwoColors(nums, 1, lastZeroIndex);
 }
