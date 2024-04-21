fcaccbb8570c1d46c44dc2b5cd609c18939fefaa
Toan Tran <toantdfu@gmail.com>
2024-04-21 16:52:50 +0900

    Time: 241 ms (13.18%), Space: 80.4 MB (52.99%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -2,18 +2,23 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-var eraseOverlapIntervals = function(intervals) {
-  if (intervals.length < 1) return 0;
-  intervals.sort((a, b) => a[1] - b[1]);
-  
-  let count = 0;
-  let end = intervals[0][1];
-  for (let i = 1; i < intervals.length; i++) {
-    if (intervals[i][0] < end)
-      count++;
-    else
-      end = intervals[i][1];
-  }
-  
-  return count;
+function eraseOverlapIntervals(intervals) {
+    intervals.sort(([start1, end1], [start2, end2]) => end1 - end2);
+
+    console.log(intervals);
+
+    let i = 0;
+    let numOfIntervals = 0;
+
+    while (i < intervals.length) {
+        let currentInterval = intervals[i];
+
+        while (intervals[i + 1] && currentInterval[1] > intervals[i + 1][0]) {
+            numOfIntervals++;
+            i++;
+        }
+        i++;
+    }
+
+    return numOfIntervals;
 };
