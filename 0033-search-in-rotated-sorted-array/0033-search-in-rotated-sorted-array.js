d91af22405c3214a1c4470f5da674d0349f873cf
Toan Tran <toantdfu@gmail.com>
2024-04-24 09:14:46 +0900

    Time: 57 ms (31.97%), Space: 48.4 MB (90.89%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function search(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        const mid = l + Math.floor((r - l) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[l] <= nums[mid]) {
+            if (nums[l] <= target && target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+};
