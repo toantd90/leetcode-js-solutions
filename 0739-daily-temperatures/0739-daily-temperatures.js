16e051e5e441c85bd1e7b6ca05fe53789d6caee2
Toan Tran <toantdfu@gmail.com>
2024-04-25 09:13:14 +0900

    Time: 254 ms (13.62%), Space: 75.7 MB (29.66%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,22 +3,21 @@
  * @return {number[]}
  */
 function dailyTemperatures(temperatures) {
-  let result = new Array(temperatures.length).fill(0);
-  let stack = [temperatures.length - 1];
+    const n = temperatures.length;
+    let answer = new Array(n).fill(0);
+    let stack = [n - 1]
 
-  for (let i = temperatures.length - 2; i >= 0; i--) {
-    while (
-      stack.length > 0 &&
-      temperatures[i] >= temperatures[stack[stack.length - 1]]
-    ) {
-      stack.pop();
+    for (let i = n - 2; i >= 0; i--) {
+        while (temperatures[i] >= temperatures[stack[stack.length - 1]]) {
+            stack.pop();
+        }
+
+        if (stack.length) {
+            answer[i] = stack[stack.length - 1] - i;
+        }
+
+        stack.push(i);
     }
 
-    if (stack.length > 0) {
-      result[i] = stack[stack.length - 1] - i;
-    }
-    stack.push(i);
-  }
-
-  return result;
-}
+    return answer;
+};
