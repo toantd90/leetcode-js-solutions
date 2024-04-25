2dc8152e718ca64baed0e184e7ebf348a1098863
Toan Tran <toantdfu@gmail.com>
2024-04-25 09:35:24 +0900

    Time: 72 ms (75.56%), Space: 53.7 MB (25.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -2,31 +2,27 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const setZeroes = (matrix) => {
-  const m = matrix.length;
-  const n = matrix[0].length;
-
-  let rows = new Set();
-  let cols = new Set();
-
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (matrix[i][j] === 0) {
-        rows.add(i);
-        cols.add(j);
-      }
-    }
-  }
-
-  rows.forEach(row => {
-    for (let j = 0; j < n; j++) {
-      matrix[row][j] = 0;
-    }
-  });
-
-  cols.forEach(col => {
+function setZeroes(matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    let rows = new Array(m).fill(false);
+    let cols = new Array(m).fill(false);
+    
     for (let i = 0; i < m; i++) {
-      matrix[i][col] = 0;
+        for (let j = 0; j < n; j++) {
+            if (!matrix[i][j]) {
+                rows[i] = true;
+                cols[j] = true;
+            }
+        }
     }
-  });
+
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (rows[i] || cols[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
 };
