6c488ec6da1ba1b3a2ee5c68b2b718bcccb8da19
Toan Tran <toantdfu@gmail.com>
2024-05-07 08:46:39 +0900

    Time: 45 ms (99.62%), Space: 50.2 MB (75.94%) - LeetHub

--- 3136-valid-word/3136-valid-word.js
+++ 3136-valid-word/3136-valid-word.js
@@ -1,35 +1,36 @@
-function isInRange(charCode, lowerRange, upperRange) {
-    return charCode >= lowerRange && charCode <= upperRange;
-}
-
-function isVowel(letter) {
-    return ['a', 'e', 'i', 'o', 'u'].includes(letter.toLowerCase());
-}
-
 /**
  * @param {string} word
  * @return {boolean}
  */
-function isValid(word) {
+var isValid = function (word) {
     if (word.length < 3) return false;
-    let hasConsonant = false;
-    let hasVowel = false;
-    for (let i = 0; i < word.length; i++) {
-        const charCode = word.charCodeAt(i);
-        const isDigit = isInRange(charCode, 48, 57);
-        const isEnglishLetter = isInRange(charCode, 65, 90) || isInRange(charCode, 97, 122);
-        if (!isDigit && !isEnglishLetter) {
-            return false;
-        }
 
-        if (!isDigit) {
-            if (isVowel(word[i])) {
-                hasVowel = true;
-            } else {
-                hasConsonant = true;
-            }
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const chars = ['@', '#', '$'];
+
+    let hasVowel = false;
+    let hasNum = false;
+    let hasConsonant = false;
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        if (nums.includes(word[i])) continue;
+
+        const charCode = word.charCodeAt(i);
+        if (vowels.includes(word[i])) {
+            hasVowel = true;
+            count++; ``
+        } else if (((charCode >= 97 && charCode <= 122) || (charCode >= 65 && charCode <= 90))) {
+            hasConsonant = true;
+            count++;
+        } else {
+            return false;
         }
     }
 
-    return hasConsonant && hasVowel;
+    if (hasVowel && hasConsonant) return true;
+
+
+    return false;
 };
