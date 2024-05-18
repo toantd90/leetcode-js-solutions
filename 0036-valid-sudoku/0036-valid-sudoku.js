f53b2de6f1d2ee9bbe48b2626488bf2ef2b18fc3
Toan Tran <toantdfu@gmail.com>
2024-05-19 08:49:23 +0900

    Time: 73 ms (40.35%), Space: 53.8 MB (32.76%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -2,32 +2,30 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {
+function isValidSudoku(board) {
+  let cols = new Array(9).fill(null).map(() => new Set());
+  let rows = new Array(9).fill(null).map(() => new Set());
+  let boxes = new Array(9).fill(null).map(() => new Set());
+
   for (let i = 0; i < 9; i++) {
-    let row = new Set(),
-      col = new Set(),
-      box = new Set();
-
     for (let j = 0; j < 9; j++) {
-      let _row = board[i][j];
-      let _col = board[j][i];
-      let _box =
-        board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
-
-      if (_row != '.') {
-        if (row.has(_row)) return false;
-        row.add(_row);
+      const boxIndex = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+      const cell = board[i][j];
+      if (cell === ".") {
+        continue;
       }
-      if (_col != '.') {
-        if (col.has(_col)) return false;
-        col.add(_col);
+      if (
+        rows[i].has(cell) ||
+        cols[j].has(cell) ||
+        boxes[boxIndex].has(cell)
+      ) {
+        return false;
       }
-
-      if (_box != '.') {
-        if (box.has(_box)) return false;
-        box.add(_box);
-      }
+      rows[i].add(cell);
+      cols[j].add(cell);
+      boxes[boxIndex].add(cell);
     }
   }
+
   return true;
-};
+}
