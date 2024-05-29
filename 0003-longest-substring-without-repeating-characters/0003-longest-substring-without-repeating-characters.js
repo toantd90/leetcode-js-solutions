faee999e7b13fd6f209d03d27b3f1ee757eabcdb
Toan Tran <toantdfu@gmail.com>
2024-05-30 08:51:38 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,43 +2,28 @@
  * @param {string} s
  * @return {number}
  */
-// var lengthOfLongestSubstring = function(s) {
-//   let left = 0;
-//   let counts = {};
-//   let longest = 0;
-  
-//   for (let right = 0; right < s.length;) {
-//     counts[s[right]] = (counts[s[right]] || 0) + 1;
+const lengthOfLongestSubstring = s => {
+    if (!s.length) return 0
+    let l = 0
+    let r = 1
+    let curMap = {}
+    curMap[s[0]] = true
+    let cnt = 1
     
-//     if (Object.values(counts).some(el => el > 1)) {
-//       counts[s[left]]--;
-//       left++;
-//     }
     
-//     longest = Math.max(longest, right - left + 1);
-//     right++;
-//   }
-  
-//   return longest;
-// };
-
-var lengthOfLongestSubstring = function(s) {
-  let left = 0;
-  let right = 0;
-  let chars = new Array(128).fill(false);
-  let longest = 0;
-  
-  while (right < s.length) {
-    if (!chars[s.charCodeAt(right)]) {
-      longest = Math.max(longest, right - left + 1);
-      chars[s.charCodeAt(right)] = true;
-    } else {
-      if (s[left] !== s[right])
-        chars[s.charCodeAt(left)] = false;
-      left++;
+    while (r < s.length) {
+      if (!curMap[s[r]]) {
+          curMap[s[r]] = true
+          cnt = Math.max(cnt, r - l + 1)
+          r++
+      } else {
+          while (s[l] !== s[r]) {
+              l++
+          }
+          delete curMap[s[r]]
+          l++
+      }
     }
-    right++;
-  }
-  
-  return longest;
-}
+    
+    return cnt
+};
