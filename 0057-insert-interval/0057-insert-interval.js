de1e6306f8c858720f5e6ae16c9d296628e288f6
Toan Tran <toantdfu@gmail.com>
2024-06-08 11:02:49 +0900

    Time: 73 ms (65.95%), Space: 52.9 MB (91.41%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -5,39 +5,20 @@
  */
 var insert = function (intervals, newInterval) {
     let newIntervals = [];
+    let i = 0;
 
-    if (!intervals.length) {
-        return [newInterval];
+    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+        newIntervals.push(intervals[i++]);
     }
 
-    if (newInterval[1] < intervals[0][0]) {
-        return [newInterval, ...intervals];
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+        newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+        i++;
     }
 
-    if (newInterval[0] > intervals[intervals.length - 1][1]) {
-        return [...intervals, newInterval];
-    }
-
-    for (let i = 0; i < intervals.length; i++) {
-        if (intervals[i][1] < newInterval[0] || intervals[i][0] > newIntervals[1]) {
-            newIntervals.push(intervals[i]);
-        } else {
-            if (intervals[i][0] <= newInterval[0] && intervals[i][1] >= newInterval[1]) {
-                return intervals;
-            }
-
-            let mergedInteral = newInterval;
-            while (intervals[i] && (intervals[i][0] <= mergedInteral[1] || intervals[i][1] <= mergedInteral[0])) {
-                mergedInteral[0] = Math.min(intervals[i][0], mergedInteral[0]);
-                mergedInteral[1] = Math.max(intervals[i][1], mergedInteral[1]);
-                i++;
-            }
-
-            newIntervals.push(mergedInteral);
-            newIntervals.push(...intervals.slice(i))
-            return newIntervals;
-        }
-    }
+    newIntervals.push(newInterval);
+    newIntervals.push(...intervals.slice(i));
 
     return newIntervals;
 };
