50bd6039fa01c9bb8c2c67bc7847e4f28c9435a4
Toan Tran <toantdfu@gmail.com>
2024-06-12 09:09:22 +0900

    Time: 88 ms (82.63%), Space: 60.9 MB (15.49%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.js
@@ -1,19 +1,19 @@
-function canMakeBouquet(bloomDay, m, k, makeDay) {
-  let consecutiveMakableDay = 0;
-  let bouquet = 0;
+function canMakeBouquets(bloomDay, m, k, day) {
+    let flowers = 0;
+    let bouquets = 0;
 
-  for (let day of bloomDay) {
-    if (day <= makeDay) {
-      consecutiveMakableDay++;
-    } else {
-      bouquet += Math.floor(consecutiveMakableDay / k);
-      consecutiveMakableDay = 0;
+    for (let bDay of bloomDay) {
+        if (day >= bDay) {
+            flowers++;
+        } else {
+            bouquets += Math.floor(flowers / k);
+            flowers = 0;
+        }
     }
-  }
 
-  bouquet += Math.floor(consecutiveMakableDay / k);
+    bouquets += Math.floor(flowers / k);
 
-  return bouquet >= m;
+    return bouquets >= m;
 }
 
 /**
@@ -23,23 +23,22 @@ function canMakeBouquet(bloomDay, m, k, makeDay) {
  * @return {number}
  */
 var minDays = function (bloomDay, m, k) {
-  if (m * k > bloomDay.length) return -1;
-  let l = Number.MAX_SAFE_INTEGER;
-  let r = 0;
-  for (let day of bloomDay) {
-    l = Math.min(l, day);
-    r = Math.max(r, day);
-  }
+    const n = bloomDay.length;
 
-  while (l < r) {
-    const mid = l + Math.floor((r - l) / 2);
+    if (m * k > n) return -1;
 
-    if (canMakeBouquet(bloomDay, m, k, mid)) {
-      r = mid;
-    } else {
-      l = mid + 1;
+    let l = Math.min(...bloomDay);
+    let r = Math.max(...bloomDay);
+
+    while (l < r) {
+        const mid = l + Math.floor((r - l) / 2);
+
+        if (canMakeBouquets(bloomDay, m, k, mid)) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
     }
-  }
 
-  return l;
+    return l;
 };
