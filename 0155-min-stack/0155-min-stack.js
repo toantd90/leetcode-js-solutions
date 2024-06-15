1df78edc68c363a2b455d3e18bfa6e0ba5dce9a6
Toan Tran <toantdfu@gmail.com>
2024-06-16 08:50:46 +0900

    Time: 94 ms (40.54%), Space: 59.3 MB (40.47%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,59 +1,51 @@
-/**
- * initialize your data structure here.
- */
-var MinStack = function() {
-  this.data = [];
+
+var MinStack = function () {
+    this.stack = [];
 };
 
-/**
- * @param {number} x
+/** 
+ * @param {number} val
  * @return {void}
  */
-MinStack.prototype.push = function(x) {
-  const min = this.getMin();
-  this.data.push({
-    value: x,
-    min: typeof x !== 'number' 
-      ? min
-      : typeof min === 'number'
-        ? x < min
-          ? x
-          : min
-        : x
-  });
+MinStack.prototype.push = function (val) {
+    if (this.stack.length) {
+        const currentMin = this.stack[this.stack.length - 1].min;
+        if (val < currentMin) {
+            this.stack.push(({ val, min: val }));
+        } else {
+            this.stack.push(({ val, min: currentMin }));
+        }
+    } else {
+        this.stack.push(({ val, min: val }));
+    }
 };
 
 /**
  * @return {void}
  */
-MinStack.prototype.pop = function() {
-  this.data.pop();
+MinStack.prototype.pop = function () {
+    this.stack.pop();
 };
 
 /**
  * @return {number}
  */
-MinStack.prototype.top = function() {
-  return this.getLast().value;
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1].val;
 };
 
 /**
  * @return {number}
  */
-MinStack.prototype.getMin = function() {
-  return this.getLast() ? this.getLast().min : null;
+MinStack.prototype.getMin = function () {
+    return this.stack[this.stack.length - 1].min;
 };
 
-MinStack.prototype.getLast = function() {
-  return this.data[this.data.length - 1];
-};
-
-/**
+/** 
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
- * obj.push(x)
+ * obj.push(val)
  * obj.pop()
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
-    
