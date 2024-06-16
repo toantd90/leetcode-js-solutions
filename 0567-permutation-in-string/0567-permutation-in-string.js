cc0a97b3fb009aa7452fc2866ce89893062df5ce
Toan Tran <toantdfu@gmail.com>
2024-06-17 08:40:21 +0900

    Time: 83 ms (42.26%), Space: 58 MB (10.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -1,28 +1,33 @@
+function isPermutation(charMap) {
+    return [...charMap.values()].every(count => count === 0);
+}
+
 /**
  * @param {string} s1
  * @param {string} s2
  * @return {boolean}
  */
-const checkInclusion = (s1, s2) => {
-    let count = new Array(26).fill(0)
-    const s1Len = s1.length
-    
-    for (let i = 0; i < s1Len; i++) {
-        count[s1.charCodeAt(i) - 97]++
-        count[s2.charCodeAt(i) - 97]--
-    }
-    
-    if (isAllZero(count)) return true
-    
-    for (let i = s1Len; i < s2.length; i++) {
-       count[s2.charCodeAt(i - s1Len) - 97]++
-       count[s2.charCodeAt(i) - 97]--
-        
-       if (isAllZero(count)) return true
-    }
-    
-    return false
-    
-};
+var checkInclusion = function (s1, s2) {
+    if (s1.length > s2.length) return false;
+    let charMap = new Map();
 
-const isAllZero = arr => arr.every(el => el === 0)
+    for (let i = 0; i < s1.length; i++) {
+        charMap.set(s1[i], (charMap.get(s1[i]) || 0) + 1);
+        charMap.set(s2[i], (charMap.get(s2[i]) || 0) - 1);
+    }
+
+    if (isPermutation(charMap)) {
+        return true;
+    }
+
+    for (let i = s1.length; i < s2.length; i++) {
+        charMap.set(s2[i], (charMap.get(s2[i]) || 0) - 1);
+        charMap.set(s2[i - s1.length], (charMap.get(s2[i - s1.length]) || 0) + 1);
+
+        if (isPermutation(charMap)) {
+            return true;
+        }
+    }
+
+    return false;
+};
