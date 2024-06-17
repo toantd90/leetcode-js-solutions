1f35b08c3b3d6c8a0f9f9c9d0288c7eaa9ea18ae
Toan Tran <toantdfu@gmail.com>
2024-06-18 08:39:38 +0900

    Time: 77 ms (31.69%), Space: 58.1 MB (29.15%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,15 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-function maxSubArray(nums) {
-    let cur = nums[0];
-    let max = nums[0];
+var maxSubArray = function (nums) {
+    let curSum = nums[0];
+    let maxSum = nums[0];
 
     for (let i = 1; i < nums.length; i++) {
-        cur = Math.max(nums[i], cur + nums[i]);
-        max = Math.max(max, cur);
+        curSum = Math.max(curSum + nums[i], nums[i]);
+        maxSum = Math.max(maxSum, curSum);
     }
 
-    return max;
+    return maxSum;
 };
-
