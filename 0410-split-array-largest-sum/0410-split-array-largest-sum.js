59d15bad26cfddc0ff350c69b69341a24f20ebe4
Toan Tran <toantdfu@gmail.com>
2024-06-23 06:32:59 +0900

    Time: 46 ms (90.97%), Space: 48.7 MB (83.75%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.js
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.js
@@ -1,23 +1,20 @@
 function canSplit(nums, k, sum) {
-    let curSum = 0;
     let i = 0;
-    let numOfSubArrays = 1;
+    let curSum = 0;
+    let numOfSubArr = 1;
 
-    while (i < nums.length) {
+    while (i < nums.length && numOfSubArr <= k) {
         curSum += nums[i];
 
         if (curSum > sum) {
             curSum = nums[i];
-            numOfSubArrays++;
-
-            if (numOfSubArrays > k) {
-                return false;
-            }
+            numOfSubArr++;
         }
+
         i++;
     }
 
-    return numOfSubArrays <= k;
+    return numOfSubArr <= k;
 }
 
 /**
@@ -27,10 +24,11 @@ function canSplit(nums, k, sum) {
  */
 var splitArray = function (nums, k) {
     let l = 0;
-    let r = nums.reduce((acc, num) => {
+    let r = 0;
+    for (num of nums) {
         l = Math.max(l, num);
-        return acc + num
-    });
+        r += num;
+    }
 
     while (l < r) {
         const mid = l + Math.floor((r - l) / 2);
