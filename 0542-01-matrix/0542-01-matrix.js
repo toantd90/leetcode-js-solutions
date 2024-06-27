4bcc422a9a6df56dc9ec7a39c4036682fd4fc96d
Toan Tran <toantdfu@gmail.com>
2024-06-28 06:26:56 +0900

    Time: 129 ms (81.39%), Space: 58.8 MB (77.16%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -2,35 +2,48 @@
  * @param {number[][]} mat
  * @return {number[][]}
  */
-const updateMatrix = mat => {
-    const m = mat.length
-    const n = mat[0].length
-    
-    const isInRange = (r, c) => r >= 0 && r < m && c >= 0 && c < n
-    const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-    
-    let queue = []
-    
+var updateMatrix = function (mat) {
+    const m = mat.length;
+    const n = mat[0].length;
+
+    const directions = [0, 1, 0, -1, 0];
+    const isInGrid = (x, y) => x >= 0 && x < m && y >= 0 && y < n;
+
+    let queue = [];
+
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (!mat[i][j]) queue.push([i , j])
-            else mat[i][j] = -1
-        }
-    }
-    
-    while (queue.length) {
-        const [r, c] = queue.shift()
-        
-        for (let [addR, addC] of directions) {
-            const desR = r + addR
-            const desC = c + addC
-            
-            if (isInRange(desR, desC) && mat[desR][desC] === -1) {
-                mat[desR][desC] = mat[r][c] + 1
-                queue.push([desR, desC])
+            if (mat[i][j]) {
+                mat[i][j] = -1;
+            } else {
+                queue.push([i, j]);
             }
         }
     }
-    
-    return mat
+
+    while (queue.length) {
+        const size = queue.length;
+
+        for (let i = 0; i < size; i++) {
+            const [x, y] = queue[i];
+
+            for (let j = 0; j < 4; j++) {
+                const addX = directions[j];
+                const addY = directions[j + 1];
+
+                const newX = x + addX;
+                const newY = y + addY;
+
+                if (isInGrid(newX, newY) && mat[newX][newY] === -1) {
+                    mat[newX][newY] = mat[x][y] + 1;
+                    queue.push([newX, newY])
+                }
+
+            }
+        }
+
+        queue.splice(0, size);
+    }
+
+    return mat;
 };
