464cb0f634775a2ea6170519de3a999b4d1bc4fb
Toan Tran <toantdfu@gmail.com>
2024-07-01 06:55:39 +0900

    Time: 124 ms (36.34%), Space: 64.9 MB (6.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,18 +2,18 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-function groupAnagrams(strs) {
-  let groupStr = {};
+var groupAnagrams = function (strs) {
+    let map = {};
 
-  for (let str of strs) {
-    const sorted = str.split("").toSorted().join("");
+    for (let str of strs) {
+        const sortedStr = str.split('').sort().join('');
+        if (!map[sortedStr]) {
+            map[sortedStr] = [];
+        }
 
-    if (groupStr[sorted]) {
-      groupStr[sorted].push(str);
-    } else {
-      groupStr[sorted] = [str];
+        map[sortedStr].push(str);
     }
-  }
 
-  return Object.entries(groupStr).map(([_, values]) => [...values]);
-}
+
+    return Object.values(map);
+};
