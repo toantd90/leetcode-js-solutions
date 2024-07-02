779d81a82c7d0f7fd6a886b8efeddc16dcd5bd51
Toan Tran <toantdfu@gmail.com>
2024-07-03 08:42:59 +0900

    Time: 55 ms (95.72%), Space: 54.8 MB (38.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -3,34 +3,18 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function (nums, k) {
-    const n = nums.length;
-    let count = new Map();
+function topKFrequent(nums, k) {
+    let countMap = new Map();
 
     for (let num of nums) {
-        if (!count.get(num)) {
-            count.set(num, 1);
-        } else {
-            count.set(num, count.get(num) + 1);
-        }
+        countMap.set(num, {
+            num,
+            frequent: (countMap.get(num)?.frequent || 0) + 1,
+        });
     }
 
-    let bucket = new Array(n + 1).fill().map((_) => new Array());
-    
-    count.forEach((value, key) => {
-        bucket[value].push(key);
-    });
-
-    let result = []
-    for (let i = n; i > 0; i--) {
-        if (bucket[i].length > 0) {
-            result.push(...bucket[i]);
-
-            if (result.length >= k) {
-                break;
-            }
-        }
-    }
-
-    return result;
-};
+    return Array.from(countMap.values())
+        .sort((count1, count2) => count2.frequent - count1.frequent)
+        .slice(0, k)
+        .map((count) => count.num);
+}
