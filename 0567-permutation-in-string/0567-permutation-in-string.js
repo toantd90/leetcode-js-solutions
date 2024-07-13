1aa2fb4a1928b99f484809ea710838d08d2a6da7
Toan Tran <toantdfu@gmail.com>
2024-07-14 06:54:04 +0900

    Time: 60 ms (94.25%), Space: 43.5 MB (100%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -1,46 +1,28 @@
-function isPermutation(a1, a2) {
-    for (let i = 0; i < a1.length; i++) {
-        if (a1[i] !== a2[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 /**
  * @param {string} s1
  * @param {string} s2
  * @return {boolean}
  */
-var checkInclusion = function (s1, s2) {
-    if (s1.length > s2.length) return false;
-
-    let s1Count = new Array(26).fill(0);
-    let s2Count = new Array(26).fill(0);
-
-
-    for (let i = 0; i < s1.length; i++) {
-        const s1CharIndex = s1.charCodeAt(i) - 97;
-        s1Count[s1CharIndex]++;
-        const s2CharIndex = s2.charCodeAt(i) - 97;
-        s2Count[s2CharIndex]++;
+const checkInclusion = (s1, s2) => {
+    let count = new Array(26).fill(0)
+    const s1Len = s1.length
+    
+    for (let i = 0; i < s1Len; i++) {
+        count[s1.charCodeAt(i) - 97]++
+        count[s2.charCodeAt(i) - 97]--
     }
-
-    if (isPermutation(s1Count, s2Count))
-        return true;
-
-
-    for (let i = s1.length; i < s2.length; i++) {
-        let s2CharIndex = s2.charCodeAt(i - s1.length) - 97;
-        s2Count[s2CharIndex]--;
-        s2CharIndex = s2.charCodeAt(i) - 97;
-        s2Count[s2CharIndex]++;
-
-        if (isPermutation(s1Count, s2Count)) {
-            return true;
-        }
+    
+    if (isAllZero(count)) return true
+    
+    for (let i = s1Len; i < s2.length; i++) {
+       count[s2.charCodeAt(i - s1Len) - 97]++
+       count[s2.charCodeAt(i) - 97]--
+        
+       if (isAllZero(count)) return true
     }
-
-    return false;
+    
+    return false
+    
 };
+
+const isAllZero = arr => arr.every(el => el === 0)
