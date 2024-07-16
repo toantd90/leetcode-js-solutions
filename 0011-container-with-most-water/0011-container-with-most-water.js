2d7fdcf4022d14078a90cd93274df463c7efea21
Toan Tran <toantdfu@gmail.com>
2024-07-17 08:46:09 +0900

    Time: 74 ms (27.27%), Space: 57 MB (49.81%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var maxArea = function (height) {
+    let max = 0;
+
     let l = 0;
     let r = height.length - 1;
-    let max = 0
 
     while (l < r) {
         if (height[l] < height[r]) {
