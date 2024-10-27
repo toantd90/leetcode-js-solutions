9a11be38d8b00fa79ecb556ca13245730f03d945
Toan Tran <toantdfu@gmail.com>
2024-10-28 07:56:27 +0900

    Time: 536 ms (5.21%), Space: 60.3 MB (7.25%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -1,34 +1,38 @@
-const operators = {
-  "+": function (a, b) {
-    return a + b;
-  },
-  "-": function (a, b) {
-    return a - b;
-  },
-  "*": function (a, b) {
-    return a * b;
-  },
-  "/": function (a, b) {
-    return Math.trunc(a / b);
-  },
-};
+function isNum(str) {
+    return !['+', '-', '*', '/'].includes(str);
+}
+
+function calculate(num1, num2, operator) {
+    console.log(num1, num2, operator);
+    if (operator === '+') {
+        return num1 + num2;
+    } else if (operator === '-') {
+        return num1 - num2;
+    } else if (operator === '*') {
+        return num1 * num2;
+    } else {
+        return Math.trunc(num1 / num2);
+    }
+
+}
 
 /**
  * @param {string[]} tokens
  * @return {number}
  */
 var evalRPN = function (tokens) {
-  let stack = [];
-  for (let token of tokens) {
-    if (operators[token]) {
-      const num2 = stack.pop();
-      const num1 = stack.pop();
-      const res = operators[token](num1, num2);
-      stack.push(res);
-    } else {
-      stack.push(Number(token));
-    }
-  }
+    let stack = [];
 
-  return stack.pop();
+    for (let token of tokens) {
+        if (isNum(token)) {
+            stack.push(Number(token));
+        } else {
+            const num2 = stack.pop();
+            stack.push(calculate(stack.pop(), num2, token));
+        }
+
+        console.log(stack);
+    }
+
+    return stack[0];
 };
