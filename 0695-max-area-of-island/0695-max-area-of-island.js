1c42b76520bb52829186046136e88f8c3cd1b278
Toan Tran <toantdfu@gmail.com>
2024-11-05 10:36:55 +0900

    Time: 4 ms (70.05%), Space: 51.9 MB (74.17%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -1,51 +1,37 @@
-function isInGrid(rows, cols, row, col) {
-    return row >= 0 && row < rows && col >= 0 && col < cols;
-}
-
-const directions = [0, 1, 0, -1, 0]
-
-function dfs(grid, row, col, visited, area) {
-    visited.add(`${row}-${col}`);
-    grid[row][col] = 0;
-    area++;
-    for (let i = 0; i < 4; i++) {
-        const nextRow = row + directions[i];
-        const nextCol = col + directions[i + 1];
-        if (isInGrid(grid.length, grid[0].length, nextRow, nextCol) && grid[nextRow][nextCol] === 1) {
-            area += dfs(grid, nextRow, nextCol, visited, 0);
-        }
-    }
-
-    return area;
-}
-
 /**
  * @param {number[][]} grid
  * @return {number}
  */
-function maxAreaOfIsland(grid) {
-    let maxArea = 0;
+var maxAreaOfIsland = function(grid) {
+    let biggestIslandArea = 0;
+    const n = grid.length
+    const m = grid[0].length
+    const LAND = 1;
+    
+    let depth = 0
+    const dfs = (i, j) => {
+      if (i < 0 || i >= n) return false
+      if (j < 0 || j >= m) return false
+      if (grid[i][j] != LAND) return false
+  
+      grid[i][j] = 0
+      if(dfs(i+1, j, depth)) depth++
+      if(dfs(i-1, j, depth)) depth++
+      if(dfs(i, j+1, depth)) depth++
+      if(dfs(i, j-1, depth)) depth++
+      if (depth > biggestIslandArea)
+        biggestIslandArea = depth
+      return true
+    }
 
-    const rows = grid.length;
-    const cols = grid[0].length;
-
-    let oneCells = [];
-
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < cols; col++) {
-            if (grid[row][col] === 1) {
-                oneCells.push([row, col]);
-            }
+    
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        if (grid[i][j] == LAND) {
+          depth = 1
+          dfs(i, j)
         }
+      }
     }
-
-    let visited = new Set();
-    for (let [row, col] of oneCells) {
-        if (visited.has(`${row}-${col}`)) continue;
-
-        currentArea = dfs(grid, row, col, visited, 0);
-        maxArea = Math.max(maxArea, currentArea);
-    }
-
-    return maxArea;
+    return biggestIslandArea;
 };
