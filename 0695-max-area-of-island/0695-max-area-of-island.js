d31738793a87ce69cdafe653a320521a95ac46aa
Toan Tran <toantdfu@gmail.com>
2024-11-05 10:24:51 +0900

    Time: 11 ms (31.8%), Space: 55.6 MB (28.71%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -1,44 +1,51 @@
+function isInGrid(rows, cols, row, col) {
+    return row >= 0 && row < rows && col >= 0 && col < cols;
+}
+
+const directions = [0, 1, 0, -1, 0]
+
+function dfs(grid, row, col, visited, area) {
+    visited.add(`${row}-${col}`);
+    grid[row][col] = 0;
+    area++
+    for (let i = 0; i < 4; i++) {
+        const nextRow = row + directions[i];
+        const nextCol = col + directions[i + 1];
+        if (isInGrid(grid.length, grid[0].length, nextRow, nextCol) && grid[nextRow][nextCol] === 1) {
+            area += dfs(grid, nextRow, nextCol, visited, 0);
+        }
+    }
+
+    return area;
+}
+
 /**
  * @param {number[][]} grid
  * @return {number}
  */
-const maxAreaOfIsland = grid => {
-    let maxArea = 0
-    const m = grid.length
-    const n = grid[0].length
-    
-    const isInGrid = (row, col) => row >= 0 && row < m && col >= 0 && col < n
-    const dirs = [0, 1, 0, -1, 0]
-    
-    
-    const dfs = (row, col) => {
-        let area = 1
-        let stack = [[row, col]]
-        while (stack.length) {
-            const [row, col] = stack.shift()
+function maxAreaOfIsland(grid) {
+    let maxArea = 0;
 
-            for (let i = 0; i < 4; i++) {
-                const nextRow = row + dirs[i]
-                const nextCol = col + dirs[i + 1]
-                if (isInGrid(nextRow, nextCol) && grid[nextRow][nextCol]) {
-                    grid[nextRow][nextCol] = 0
-                    area++
-                    stack.push([nextRow, nextCol])
-                }
-            }
-        }
-        
-        return area
-    }
-    
-    for (let row = 0; row < m; row++) {
-        for (let col = 0; col < n; col++) {
-            if (grid[row][col]) {
-                grid[row][col] = 0
-                maxArea = Math.max(maxArea, dfs(row, col))
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+    let oneCells = [];
+
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (grid[row][col] === 1) {
+                oneCells.push([row, col]);
             }
         }
     }
-    
-    return maxArea
+
+    let visited = new Set();
+    for (let [row, col] of oneCells) {
+        if (visited.has(`${row}-${col}`)) continue;
+
+        currentArea = dfs(grid, row, col, visited, 0);
+        maxArea = Math.max(maxArea, currentArea);
+    }
+
+    return maxArea;
 };
