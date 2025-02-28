7a7e4dec8ccc5474fe0a5a7983912d6c5d99d7ed
Toan Tran <toantdfu@gmail.com>
2025-03-01 05:06:33 +0900

    Time: 3 ms (56.17%), Space: 65.2 MB (10.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -7,11 +7,9 @@ function maxProfit(prices) {
     let profit = 0;
 
     for (let i = 1; i < prices.length; i++) {
-        if (prices[i] > min) {
-            profit = Math.max(profit, prices[i] - min);
-        } else {
-            min = prices[i];
-        }
+        min = Math.min(min, prices[i]);
+
+        profit = Math.max(profit, prices[i] - min);
     }
 
     return profit;
