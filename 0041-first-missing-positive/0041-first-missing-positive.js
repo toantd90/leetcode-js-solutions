fb177446ecb72cffe3dafcb4acd6f47820e09846
Toan Tran <toantdfu@gmail.com>
2025-03-06 17:07:35 +0900

    Time: 71 ms (5.21%), Space: 66.7 MB (27.6%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+    let arr = new Array(1e5 + 1).fill();
+
+    nums.forEach(num => {
+        if (num < 0 || num > nums.length) {
+        } else {
+            arr[num] = num;
+        }
+    });
+
+    console.log(arr);
+
+    for (let i = 1; i <= nums.length + 1; i++) {
+        if (!arr[i]) {
+            return i;
+        }
+    }
+
+    return nums.length;
+};
