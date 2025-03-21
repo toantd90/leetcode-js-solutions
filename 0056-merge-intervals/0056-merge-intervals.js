88f66ea1a6b74209107a697bd536d8c9bfeb3026
Toan Tran <toantdfu@gmail.com>
2025-03-21 09:12:37 +0900

    Time: 21 ms (97.26%), Space: 64.4 MB (37.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,25 +3,25 @@
  * @return {number[][]}
  */
 function merge(intervals) {
-    const sortedIntervals = intervals.toSorted((i1, i2) => i1[0] - i2[0]);
-
-    let mergedIntervals = [];
-
-    let i = 1;
-    let current = sortedIntervals[0];
-
-    while (i < sortedIntervals.length) {
-        if (sortedIntervals[i][0] > current[1]) {
-            mergedIntervals.push(current);
-            current = sortedIntervals[i];
-        } else {
-            current[1] = Math.max(current[1], sortedIntervals[i][1]);
+    const sortedIntervals = intervals.toSorted(([start1, end1], [start2, end2]) => {
+        if (start1 === start2) {
+            return end1 - end2;
         }
 
-        i++;
+        return start1 - start2;
+    });
+
+    let ans = [];
+
+    for (let i = 0; i < sortedIntervals.length; i++) {
+        let currentInterval = sortedIntervals[i];
+        while (i < sortedIntervals.length - 1 && currentInterval[1] >= sortedIntervals[i + 1][0]) {
+            currentInterval[1] = Math.max(currentInterval[1], sortedIntervals[i + 1][1]);
+            i++;
+        }
+
+        ans.push(currentInterval);
     }
 
-    mergedIntervals.push(current);
-
-    return mergedIntervals;
+    return ans;
 };
