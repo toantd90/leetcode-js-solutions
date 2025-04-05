7c7507b2e4549ce0a668476570445495b34d619e
Toan Tran <toantdfu@gmail.com>
2025-04-05 16:48:59 +0900

    Time: 104 ms (5.43%), Space: 43 MB (100%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -2,14 +2,15 @@
  * @param {number[]} cost
  * @return {number}
  */
-function minCostClimbingStairs(cost) {
-    let minCost = new Array(cost.length + 1).fill(0);
-    minCost[0] = cost[0];
-    minCost[1] = cost[1];
-
-    for (let i = 2; i < cost.length + 1; i++) {
-        minCost[i] = Math.min(minCost[i - 1], minCost[i - 2]) + (cost[i] || 0);
-    }
-
-    return minCost[cost.length];
+var minCostClimbingStairs = function(cost) {
+  let pre = cost[0]
+  let cur = cost[1]
+  
+  for (let i = 2; i <= cost.length; i++) {
+    const temp = cur
+    cur = Math.min(pre, cur) + (cost[i] || 0)
+    pre = temp
+  }
+  
+  return cur
 };
