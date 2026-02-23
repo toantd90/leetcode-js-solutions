781204fc6266afb498938235e165d37607e7be7a
Toan Tran <toantdfu@gmail.com>
2026-02-23 14:32:11 +0900

    Time: 248 ms (8.89%), Space: 79.9 MB (98.97%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.js
+++ 0853-car-fleet/0853-car-fleet.js
@@ -5,26 +5,22 @@
  * @return {number}
  */
 var carFleet = function (target, position, speed) {
-    const n = position.length;
+  const n = position.length;
 
-    const positionAndTime = position.map((p, i) => ({ time: (target - p) / speed[i], position: p }));
+  const positionAndTime = position
+    .map((p, i) => ({ time: (target - p) / speed[i], position: p }))
+    .sort((pt1, pt2) => pt1.position - pt2.position);
 
-    console.log(positionAndTime);
+  let fleet = 1;
+  let currentTime = positionAndTime[n - 1].time;
 
-    positionAndTime.sort((pt1, pt2) => pt1.position - pt2.position);
-
-
-
-    let fleet = 1;
-    let currentTime = positionAndTime[n - 1].time;
-
-    for (let i = n - 2; i >= 0; i--) {
-        const { time } = positionAndTime[i];
-        if (time > currentTime) {
-            fleet++;
-            currentTime = time;
-        }
+  for (let i = n - 2; i >= 0; i--) {
+    const { time } = positionAndTime[i];
+    if (time > currentTime) {
+      fleet++;
+      currentTime = time;
     }
+  }
 
-    return fleet;
+  return fleet;
 };
