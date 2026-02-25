f8e0831603301847bf05c76f63ac9605c32ba1d2
Toan Tran <toantdfu@gmail.com>
2026-02-25 15:42:23 +0900

    Time: 0 ms (100%), Space: 59.4 MB (40.78%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -3,35 +3,13 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
-    if (nums.length < 2) {
-        return true;
-    }
+    let goal = nums.length - 1;
 
-    let currentPosition = [0];
-    let visited = new Set();
-    visited.add(0);
-
-    while (currentPosition.length > 0) {
-        const len = currentPosition.length;
-        for (let i = 0; i < len; i++) {
-            for (let j = 1; j <= nums[currentPosition[i]]; j++) {
-                const nextPosition = currentPosition[i] + j;
-
-                if (nextPosition === nums.length - 1) {
-                    return true;
-                } else if (nextPosition < nums.length - 1) {
-                    if (!visited.has(nextPosition)) {
-                        visited.add(nextPosition);
-                        currentPosition.push(nextPosition);
-                    }
-                } else {
-                    //
-                }
-            }
+    for (let i = nums.length - 2; i >= 0; i--) {
+        if (i + nums[i] >= goal) {
+            goal = i;
         }
-
-        currentPosition = currentPosition.slice(len);
     }
 
-    return false;
+    return goal === 0;
 };
