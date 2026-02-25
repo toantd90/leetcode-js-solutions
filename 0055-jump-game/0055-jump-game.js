aa130aa66e0e4320e3b510022560c891ecfc57ab
Toan Tran <toantdfu@gmail.com>
2026-02-25 14:41:30 +0900

    Time: 509 ms (10.42%), Space: 65.4 MB (5.03%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -2,24 +2,36 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-function canJump(nums) {
-  const length = nums.length;
-  let stack = [0];
-  let visited = new Set();
-  
-  while (stack.length > 0) {
-    const currentIndex = stack.pop()
-    if (currentIndex == length - 1) {
-      return true;
+var canJump = function (nums) {
+    if (nums.length < 2) {
+        return true;
     }
-    for (let steps = 1; steps <= nums[currentIndex]; steps++) {
-      const nextIndex = currentIndex + steps 
-      if (!visited.has(nextIndex)) {
-        stack.push(nextIndex);
-        visited.add(nextIndex);
-      }
+
+    let currentPosition = [0];
+    let visited = new Set();
+    visited.add(0);
+
+    while (currentPosition.length > 0) {
+        const len = currentPosition.length;
+        for (let i = 0; i < len; i++) {
+            for (let j = 1; j <= nums[currentPosition[i]]; j++) {
+                const nextPosition = currentPosition[i] + j;
+
+                if (nextPosition === nums.length - 1) {
+                    return true;
+                } else if (nextPosition < nums.length - 1) {
+                    if (!visited.has(nextPosition)) {
+                        visited.add(nextPosition);
+                        currentPosition.push(nextPosition);
+                    }
+                } else {
+                    //
+                }
+            }
+        }
+
+        currentPosition = currentPosition.slice(len);
     }
-  }
-  
-  return false
+
+    return false;
 };
