fc70174f8101e0d4a8e29ac26054ea713cd576e1
Toan Tran <toantdfu@gmail.com>
2026-08-18 18:32:37 +0700

    Time: 38 ms (75.11%), Space: 71.1 MB (63.56%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
@@ -2,18 +2,12 @@ type Point = {
     x: number;
     y: number;
     distance: number;
-}
+};
 
 function kClosest(points: number[][], k: number): number[][] {
-    const transformedPoints = points.map(([x, y]) => ({ x, y, distance: x ** 2 + y ** 2 }));
-    const minHeap = new MinPriorityQueue<Point>(({ distance }: Point) => distance, transformedPoints);
-
-    let result = [];
-    for (let i = 0; i < k; i++) {
-        const point = minHeap.dequeue();
-        const { x, y } = point;
-        result.push([x, y]);
-    }
-
-    return result;
-};
+    return points
+        .map(([x, y]) => ({ x, y, distance: x ** 2 + y ** 2 }))
+        .sort((point1, point2) => point1.distance - point2.distance)
+        .slice(0, k)
+        .map(({ x, y }) => [x, y]);
+}
